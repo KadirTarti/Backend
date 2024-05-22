4ab08e3b9f352e50852b9550252b401d80c2591f
tarti <tartilaci@gmail.com>
2024-05-22 10:59:36 +0200

    example for refactoring

--- 22.05.2024-SQLite/test.sqlite3.sql
+++ 22.05.2024-SQLite/test.sqlite3.sql
@@ -1,2 +1,8 @@
 select * FROM Customer;
 SELECT * FROM Customer WHERE CustomerId IN  (1,4,8,45) and (Country= 'Brazil' or Country='USA') or CustomerId=45;
+
+-- daha kısa yazmaya çalışmak lazım (refactoring)
+SELECT * FROM Customer 
+WHERE Country 
+IN  ('USA','Brazil','Denmark') 
+and (CustomerId BETWEEN 24 and 30);
