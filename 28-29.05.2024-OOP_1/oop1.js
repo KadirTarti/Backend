860f03ef573bf6d1a5252bcb320ca6e83d0e5603
tarti <tartilaci@gmail.com>
2024-05-28 10:25:55 +0200

    examples - array destructuring

--- 28-29.05.2024-OOP_1/oop1.js
+++ 28-29.05.2024-OOP_1/oop1.js
@@ -49,4 +49,26 @@ const user = {
 console.log(user)
 console.log(user.fullName()) // hata 
 
+
+
+
 //^ ARRAY Destructuring
+
+const colors = ['black', 'blue', 'white', 'red', 'green']
+console.log(colors)
+
+const color1 = colors[0]
+console.log(color1)
+
+const color2 = colors[1]
+console.log(color2)
+
+const [a,b,c,z,e] = colors   // a,b,c,z,e ['black', 'blue', 'white', 'red', 'green']
+console.log(e,a,b,z,c)       // e,a,b,z,c   green     black   blue    red     white
+
+
+const [colorOne, colorTwo, ...colorList] = colors
+console.log(colorList) // [ 'white', 'red', 'green' ]
+
+const newColors = [...colorList, 'gray', 'purple']
+console.log('new colors >>>', newColors)  // --->  [ 'white', 'red', 'green', 'gray', 'purple' ]
