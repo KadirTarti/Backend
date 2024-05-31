f4582063fac834c0107781f4b16436e19d1823a1
tarti <tartilaci@gmail.com>
2024-05-31 11:33:25 +0200

    "Updated index.js to add new console log and require statements for importing and calling functions from module.js"

--- 31.05.2024-NodeJs_Modules/index.js
+++ 31.05.2024-NodeJs_Modules/index.js
@@ -1,2 +1,20 @@
 'use strict'
-console.log('modules in node js')
+console.log('index.js')
+// require('./modules/module.js')
+
+//! single
+// const testFunction= require('./modules/module')
+// testFunction(),
+
+//! multi function
+//^array
+// const [test2, test3, test4] = require('./modules/module')  //buraya çekilme değil gönderilen dosyadakie export sırasına göre atar
+// test2()
+// test3()
+// test4()
+
+//^object
+const { test4: t_D, test2, test3:t_C} = require('./modules/module') //buradaki sıraya göre atar. import ederken key ekleyebilir onunla çağırabiliriz
+t_C() //test3()
+t_D() //test4()
+test2()
---
+++ 31.05.2024-NodeJs_Modules/modules/module.js
@@ -0,0 +1,43 @@
+'use strict'
+
+console.log("this is module.js");
+
+//! single function
+const testFunction=function() {
+    console.log('this is test function')
+}
+module.exports=testFunction  
+
+//^short hand :
+module.exports=function() {
+    console.log('this is a function, what direct exported')
+}
+
+
+//! multi function
+const test2=function() {
+    console.log('2- this is test2')
+}
+  
+const test3=function() {
+    console.log('3- this is test3')
+}
+
+const test4=function() {
+    console.log('4- this is test4')
+}
+//& üç fonksiyonu aynı anda export için 1.yol - array olarak gönderme
+// module.exports=[
+//     test2,
+//     test3,
+//     test4
+// ]
+
+
+//& 2.yol object olarak gönderme
+module.exports={
+    test2,
+    test3,
+    test4,
+    pi: 3.14
+}
