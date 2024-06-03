ea7fd91299cfffa726664754c6a0f0ded0bc54c3
tarti <tartilaci@gmail.com>
2024-06-03 09:54:25 +0200

    "Updated index.js to create two separate HTTP servers, added dotenv dependency to package.json, and modified server creation to use environment variable for port"

--- 03.06.2024-Nodejs-modules/index.js
+++ 03.06.2024-Nodejs-modules/index.js
@@ -17,6 +17,25 @@ const http = require('node:http');
 
 
 const app = http.createServer((req, res)=>{
-    console.log('server is running');
+    res.end('server runned')
+    // !  res.end('bye')    - res.end yalnızca bir kez yazılmalı. 2.si yazılırsa hata verir
+    console.log(req.method)
+    // console.log('server is running');
 })
 app.listen(8000)
+
+
+const applic = http.createServer((req, res)=>{
+    res.end('server runned')
+    // !  res.end('bye')    - res.end yalnızca bir kez yazılmalı. 2.si yazılırsa hata verir
+    console.log(req.method)
+    // console.log('server is running');
+})
+applic.listen(8000)
+
+
+//! port'u elle 8000 olarak değil de env dosyası üzerinden yazmak için:
+// .env dosyasını oluşturduk. içine PORT=8000 yazdık. sonra terminalden npm i dotenv kurduk
+const dotenv = 2;
+const PORT = process.env.PORT || 8000;
+
