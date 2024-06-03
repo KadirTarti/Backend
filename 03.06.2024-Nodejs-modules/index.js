b4d85ff6e91fe1d90fc23377beedb121961837f4
tarti <tartilaci@gmail.com>
2024-06-03 09:27:54 +0200

    "Added console log statement, required 'http' module, and created an HTTP server with a callback function that logs a message when the server is running."

--- 03.06.2024-Nodejs-modules/index.js
+++ 03.06.2024-Nodejs-modules/index.js
@@ -4,5 +4,18 @@
 //!          NODE JS SERVER               */
 /* ------------------------------------- */
 
+console.log('node js server');
+const http = require('http');
 
-const http = require('node:http');
+
+// http.createServer((parameter1, parameter2) => {
+//     console.log('server is running');
+// })
+
+// 1. parameter request  -- yapılan istek
+// 2. parameter response -- alınan cevap
+
+
+const app = http.createServer((req, res)=>{
+    console.log('server is running');
+}).listen(8000)
