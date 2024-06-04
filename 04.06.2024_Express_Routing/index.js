5afdee19cb13280244aeaf1017eb7c3aa3c833d2
tarti <tartilaci@gmail.com>
2024-06-04 09:47:47 +0200

    "Added HTTP method handlers for GET, POST, PUT, PATCH, and DELETE, and commented out the original GET handler."

--- 04.06.2024_Express_Routing/index.js
+++ 04.06.2024_Express_Routing/index.js
@@ -10,9 +10,17 @@ require('dotenv').config()
 const PORT=process.env.PORT || 8000
 const HOST=process.env.HOST || '127.0.0.1'
 
-app.get('/', (req, res) => {
-    req.send('hallo an alle')
-})
+// app.get('/', (req, res) => {
+//     req.send('hallo an alle')
+// })
+
+app.get('/', (req, res) => {req.send('GET method called!')}) // listeleme ve/veya bilgi alma 
+app.post('/', (req, res) => {req.send('POST method called!')}) // yeni bir veri-bilgi kaydetme
+app.put('/', (req, res) => {req.send('PUT method called!')}) // veri güncelleme
+app.patch('/', (req, res) => {req.send('PATCH method called!')})
+app.delete('/', (req, res) => {req.send('DELETE method called!')})
+
+
 
 app.listen(PORT, () =>{
     console.log(`Beispiel app arbeitet on port http://${HOST}:${PORT}`)
