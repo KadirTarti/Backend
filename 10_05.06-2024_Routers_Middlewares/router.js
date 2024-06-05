127aea4a87e3fc9cc178d6318b0fe80fa467e7b0
tarti <tartilaci@gmail.com>
2024-06-05 09:42:56 +0200

    "Updated router.js to use express.Router, added routes for '/', '/user' with different HTTP methods, and modified console log message."

--- 10_05.06-2024_Routers_Middlewares/router.js
+++ 10_05.06-2024_Routers_Middlewares/router.js
@@ -1,12 +1,32 @@
 'use strict'
+
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
 const PORT=process.env.PORT || 8000
 const HOST=process.env.HOST || '127.0.0.1'
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+// app.get('/', (req, res) => {
+//     res.send('Hallo Welt!')
+// })
+
+/*-------------------*/
+//? Router ist spezial app für URL Kontrolle in ExpressJS
+
+const router = express.Router()
+
+router.get('/', (req,res) =>{    res.send({message: 'Hallo Welt'})  })
+
+router.route('/user')
+.get((req,res) =>{ res.send({message: 'Hallo Welt GET'})  })
+.post((req,res) =>{ res.send({message: 'Hallo Welt POST'})  })
+.put((req,res) =>{ res.send({message: 'Hallo Welt PUT'})  })
+.delete((req,res) =>{ res.send({message: 'Hallo Welt DELETE'})  })
+
+
+app.use(router);
+
 app.listen(PORT, () => {
-    console.log(`Example app listening on port http://${HOST}:${PORT}`)
+    console.log(`Beispiel app arbeitet auf port http://${HOST}:${PORT}`)
 })
