5bf0bce564ac3ca529cf4c74a810301b3348afcb
tarti <tartilaci@gmail.com>
2024-06-05 10:01:00 +0200

    "Formatted code, added semicolons, and refactored router definitions"

--- 10_05.06-2024_Routers_Middlewares/router.js
+++ 10_05.06-2024_Routers_Middlewares/router.js
@@ -1,11 +1,11 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-const PORT=process.env.PORT || 8000
-const HOST=process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || "127.0.0.1";
 
 // app.get('/', (req, res) => {
 //     res.send('Hallo Welt!')
@@ -14,19 +14,26 @@ const HOST=process.env.HOST || '127.0.0.1'
 /*-------------------*/
 //? Router ist spezial app für URL Kontrolle in ExpressJS
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req,res) =>{    res.send({message: 'Hallo Welt'})  })
+router.get("/", (req, res) => {
+  res.send({ message: "Hallo Welt" });
+});
 
-router.route('/user')
-.get((req,res) =>{ res.send({message: 'Hallo Welt GET'})  })
-.post((req,res) =>{ res.send({message: 'Hallo Welt POST'})  })
-.put((req,res) =>{ res.send({message: 'Hallo Welt PUT'})  })
-.delete((req,res) =>{ res.send({message: 'Hallo Welt DELETE'})  })
+router
+  .route("/user")
+  .get((req, res) => res.send({ message: "Hallo Welt GET" }))
+  .post((req, res) => res.send({ message: "Hallo Welt POST" }))
+  .put((req, res) => res.send({ message: "Hallo Welt PUT" }))
+  .delete((req, res) => res.send({ message: "Hallo Welt DELETE" }));
 
-
+//? Nachdem fertiges route-design, es sollte wie middleware geruft werden:
 app.use(router);
 
+//? Move to /routers/index.js
+const router = require('./routers/index.js')
+app.use(router)
+
 app.listen(PORT, () => {
-    console.log(`Beispiel app arbeitet auf port http://${HOST}:${PORT}`)
-})
+  console.log(`Beispiel app arbeitet auf port http://${HOST}:${PORT}`);
+});
