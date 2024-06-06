33334eb0859af5902642f172a1f5952df189e0b9
tarti <tartilaci@gmail.com>
2024-06-06 10:11:02 +0200

    "Updated index.js to handle HTTP requests and responses, added 'url' package dependency, and modified package-lock.json and package.json accordingly."

--- 06.06.2024_GuidedWS_with_Anthony/index.js
+++ 06.06.2024_GuidedWS_with_Anthony/index.js
@@ -1,13 +1,35 @@
-//&use strict zorunlu bir kullanım değil...! backend'de daha katı bir takip isteniyorsa yapılabilir!
+//&use strict zorunlu bir kullanım değil...! backend'de daha kuralcı bir takip isteniyorsa yapılabilir!
 
 //^server kurulumu
 // const { strict } = require('assert')
 const http = require('http')
+const { parse } = require('path')
 
-const routerHandler = ()=> {
+const routerHandler = (req, res)=> {
+    
+    console.log(req.url)
+    console.log(req.method)
+
+    //npm i url yaptık
+    const parsedUrl = url.parse(req.url, true)  // bundan sonra alttaki if bloğunu değiştirebiliyoruz
+    console.log(parsedUrl)
+
+    // if (req.url == '/' && req.method =='GET') {
+    //     res.write('Request url dogru calisti')
+    //     res.end('siparis tamam.islem bitti')
+    // }
+
+    if (parsedUrl.pathname == '/' && req.method =='GET') {
+        res.write('Request url dogru calisti')
+        res.end('')
+    } else{
+        res.write('Not Found')
+        res.end('')
+    }
 }
 
 const server = http.createServer(routerHandler)
 
 //^server ayağa kaldırma
 server.listen(3000)
+
