d38e9ea73e0856e2a0e9bc9648e4541b459d108c
tarti <tartilaci@gmail.com>
2024-06-06 11:12:11 +0200

    "Refactored Express.js code: reformatted code, removed comments, and updated route handlers for '/' and '/products' endpoints."

--- 06.06.2024_GuidedWS_with_Anthony/express.js
+++ 06.06.2024_GuidedWS_with_Anthony/express.js
@@ -1,30 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 //import
-const products = require('./products.json')
+const products = require("./products.json");
 
-
-app.listen(3001, function(){
-    console.log('server başladı')
-})
-
+app.listen(3001, function () {
+  console.log("server başladı");
+});
 
 // app.use('/', (req,res) => {
 //     res.send('send edildi')
 // })
 
-
 //& yazım sırası özelden genele olmalı. çünkü yukarıdan aşağıya doğru okuyor. önce bulduğunu getiriyor. eğer '/' path'i en üstte olursa alttaki linklere gidemez.
-
 
 // app.use('/products', (req,res) => {
 //     if(req.method =='GET') {
 //         res.send('Hallo GET Products')
 //     } else if (req.method == 'POST') {
-//         res.send('Hallo POST')      
+//         res.send('Hallo POST')
 //     } else {
-//         res.send('Req method is not GET or POST')      
+//         res.send('Req method is not GET or POST')
 //     }
 // })
 
@@ -32,34 +28,46 @@ app.listen(3001, function(){
 //     if(req.method =='GET') {
 //         res.send('Hallo GET users')
 //     } else if (req.method == 'POST') {
-//         res.send('Hallo POST')      
+//         res.send('Hallo POST')
 //     } else {
-//         res.send('Req method is not GET or POST')      
+//         res.send('Req method is not GET or POST')
 //     }
 // })
-
 
 // app.use('/', (req,res) => {
 //     if(req.method =='GET') {
 //         res.send('Hallo GET')
 //     } else if (req.method == 'POST') {
-//         res.send('Hallo POST')      
+//         res.send('Hallo POST')
 //     } else {
-//         res.send('Req method is not GET or POST')      
+//         res.send('Req method is not GET or POST')
 //     }
 // })
 
-
-app.get('/', (req, res) => {
-    res.send({message: 'app get message calisti'})
-})
+app.get("/", (req, res) => {
+  res.send({ message: "app get message calisti" });
+});
 
 // products.json dosyasından verileri çekmek için:
-app.get('/products', (req,res)=>{
-    res.send({
-        message:'this is products page',
-        products
-    })
-} )
+app.get("/products", (req, res) => {
+  console.log(req.query); // http://localhost:3001/products?page=6  {page : 6}
+
+  //    http://localhost:3001/products?page=6&limit=4     { page: '6', limit: '4' }
 
 
+
+//   const page = req.query.page || 1;
+//   const limit = req.query.limit || 10;
+  //&veya
+  const {page=1, limit=10, category='all'} = req.query
+
+
+
+  res.send({
+    message: "this is products page",
+    // products
+    // ilk 4ünü yazdırmak için
+    products: products.filter(item=>item.category == category).slice((page-1) * limit, page * limit),
+  });
+  //? üstteki kod varken sadece /product send edersek hata verir. bu durumda query varlığı doğrulanmalı. bunu da üstteki const page tanımlaası ile yapıyoruz.
+});
