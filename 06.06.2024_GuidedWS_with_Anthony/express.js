a3badd5f5d541e0db0a674c3183e778fb670dbc7
tarti <tartilaci@gmail.com>
2024-06-06 10:56:49 +0200

    "Added products import and new routes for '/' and '/products' endpoints, removed old route handlers"

--- 06.06.2024_GuidedWS_with_Anthony/express.js
+++ 06.06.2024_GuidedWS_with_Anthony/express.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+//import
+const products = require('./products.js')
+
 app.listen(3001, function(){
     console.log('server başladı')
 })
@@ -10,14 +13,51 @@ app.listen(3001, function(){
 //     res.send('send edildi')
 // })
 
-app.use('/', (req,res) => {
-    if(req.method =='GET') {
-        res.send('Hallo GET')
-    } else if (req.method == 'POST') {
-        res.send('Hallo POST')      
-    } else {
-        res.send('Req method is not GET or POST')      
-    }
+
+//& yazım sırası özelden genele olmalı. çünkü yukarıdan aşağıya doğru okuyor. önce bulduğunu getiriyor. eğer '/' path'i en üstte olursa alttaki linklere gidemez.
+
+
+// app.use('/products', (req,res) => {
+//     if(req.method =='GET') {
+//         res.send('Hallo GET Products')
+//     } else if (req.method == 'POST') {
+//         res.send('Hallo POST')      
+//     } else {
+//         res.send('Req method is not GET or POST')      
+//     }
+// })
+
+// app.use('/users', (req,res) => {
+//     if(req.method =='GET') {
+//         res.send('Hallo GET users')
+//     } else if (req.method == 'POST') {
+//         res.send('Hallo POST')      
+//     } else {
+//         res.send('Req method is not GET or POST')      
+//     }
+// })
+
+
+// app.use('/', (req,res) => {
+//     if(req.method =='GET') {
+//         res.send('Hallo GET')
+//     } else if (req.method == 'POST') {
+//         res.send('Hallo POST')      
+//     } else {
+//         res.send('Req method is not GET or POST')      
+//     }
+// })
+
+
+app.get('/', (req, res) => {
+    res.send({message: 'app get message calisti'})
 })
+
+app.get('/products', (req,res)=>{
+    res.send({
+        message:'this is products page',
+        products
+    })
+} )
 
 
