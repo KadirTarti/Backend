c4365ac580470634edff5740f0e3aca64b491e8c
tarti <tartilaci@gmail.com>
2024-06-07 09:58:20 +0200

    "Removed commented out code, updated error handling in /user/:id route, and reformatted code"

--- BE13_Node_Error_Handling_07.06.2024/error.js
+++ BE13_Node_Error_Handling_07.06.2024/error.js
@@ -6,29 +6,26 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || '127.0.0.1'
-
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
 
-app.get('/', (req,res)=>{
-    throw new Error('Error Message')
-})
+// app.get('/', (req,res)=>{
+//     throw new Error('Error Message')
+// })
 
-app.get('/user/:id', (req,res, next)=>{
+app.get('/user/:id', (req, res, next) => {
     const id = req.params?.id || 0
-
     try {
         if (isNaN(id)) {
-            
+            throw new Error('ID is not a number')
         }
-    } catch (error) {
-        
-    }
-    throw new Error('Error Message')
-    next()
-})
+    res.send({status: true})
 
+    } catch (error) {
+    }
+    res.send({status: false})
+})
 
 
 
