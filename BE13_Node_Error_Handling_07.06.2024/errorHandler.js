64e7cb71459b585a94796a8770ed54fb99c18d15
tarti <tartilaci@gmail.com>
2024-06-07 11:10:31 +0200

    "Moved error handler function from errorHandler.js to error.js and uncommented it, also removed the import of errorHandler.js in error.js"

--- BE13_Node_Error_Handling_07.06.2024/errorHandler.js
+++ BE13_Node_Error_Handling_07.06.2024/errorHandler.js
@@ -4,15 +4,13 @@
 
 
 module.exports = (err, req, res, next) => {
-    
-}
 
-const errorHandler = (err, req, res, next) => {
-    const errorStatusCode = res?.errorStatusCode || 500
-    res.status(errorStatusCode).send({
-        error: true,
-        status: false,
-        message: err.message
-    })
+    const errorHandler = (err, req, res, next) => {
+        const errorStatusCode = res?.errorStatusCode || 500
+        res.status(errorStatusCode).send({
+            error: true,
+            status: false,
+            message: err.message
+        })
+    }
 }
-app.use(errorHandler)
