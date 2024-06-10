4efbdaf85786fbb2027b8f5fdfe2d5f74fbd9687
tarti <tartilaci@gmail.com>
2024-06-10 09:30:31 +0200

    "Updated index.js to include express and dotenv setup, added error handler and json middleware, and installed express-async-error package."

--- BE14_ToDoApp/index.js
+++ BE14_ToDoApp/index.js
@@ -1,13 +1,38 @@
 'use strict'
 
-require('dotenv').config()
-
-
-const PORT=process.env?.PORT ||  8000
-const HOST=process.env?.HOST || '127.0.0.1'
+//npm i express dotenv
+// npm i express-async-error  -> asenkron hataları yakalıyor
 
 const express = require('express')
 const app = express()
+
+require('dotenv').config()
+const PORT=process.env?.PORT ||  8000
+const HOST=process.env?.HOST || '127.0.0.1'
+
+app.all('/', (req, res)=>{
+    res.send('TO DO APP')
+})
+
+
+// app.use('/todo', (req, res)=>{ //! url/todo/allurl -> (true) status code 200
+//     res.send('TO DO APP222')
+// })
+
+// json to obj - obj to json
+app.use(express.json())
+
+const errorHandler = (err, req, res, next) => {
+    const errorStatusCode = res?.errorStatusCode || 500
+    res.status(errorStatusCode).send({
+        error: true,
+        status: false,
+        message: err.message,
+        // cause: err.cause,
+        // stack: err.stack
+    })
+}
+
 
 
 app.listen(PORT,()=>console.log(`server runned http://${HOST}:${PORT}`))
