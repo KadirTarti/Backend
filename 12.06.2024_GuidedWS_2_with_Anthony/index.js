684734c978e300a354d1e13a98a9a08e6d7dfa39
tarti <tartilaci@gmail.com>
2024-06-12 11:08:59 +0200

    "Added express-async-errors dependency and required it in index.js"

--- 12.06.2024_GuidedWS_2_with_Anthony/index.js
+++ 12.06.2024_GuidedWS_2_with_Anthony/index.js
@@ -1,5 +1,7 @@
 "use strict";
 const express = require("express");
+require('express-async-errors')
+
 const app = express();
 
 app.use(express.json())
@@ -13,7 +15,6 @@ app.all("/", (req, res) => {
 app.use(require('./src/routers/contactRouter'))
 
 // errorHandler rootların bittiği yerde  tanımlanmalı
-
 app.use(require('./src/middlewares/errorHandler'))
 
 app.listen(8000, () => console.log(`server runned http://localhost:8000`));
