4d6e6d671f954ee0378fdb8ec478ab68cf574607
tarti <tartilaci@gmail.com>
2024-06-12 11:13:45 +0200

    "Updated errorHandler middleware to log error and send error response with status code and message"

--- 12.06.2024_GuidedWS_2_with_Anthony/src/middlewares/errorHandler.js
+++ 12.06.2024_GuidedWS_2_with_Anthony/src/middlewares/errorHandler.js
@@ -1,5 +1,13 @@
 //* Error Handler
 
-module.exports = (err, req, res, next) => {
+module.exports = (err,req,res,next)=>{
     console.log(err)
+    const errorStatusCode = res?.errorStatusCode || 500;
+    res.status(errorStatusCode).send({
+      error: true,
+      status: false,
+      message: err.message,
+      // cause: err.cause,
+      // stack: err.stack
+    });
 }
