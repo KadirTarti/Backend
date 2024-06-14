037c2c6b46414599206ded60c71eacfe71abb410
tarti <tartilaci@gmail.com>
2024-06-14 10:47:33 +0200

    "Refactored blogModel.js: commented out original schema, introduced new blogPostSchema with title, content, and published fields, and updated collection name and timestamps."

--- BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/models/blogModel.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/models/blogModel.js
@@ -2,25 +2,54 @@
 //import mongoose
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-    // _id : Auto generated
-    // filedName: String, //shorthand
-    fieldName:{
-        type: String, //datatype
+// const blogSchema = new mongoose.Schema({
+//     // _id : Auto generated
+//     // filedName: String, //shorthand
+//     fieldName:{
+//         type: String, //datatype
+//         default: 'true' // veri gelmezse eklenecek  değer
+//         required: true,
+//         required: [true, 'Error-message'], // error-message specification
+//         trim: true, // get data with trim
+//         unique: true,
+//         index: true, // arama yapılınca daha hızlı erişim olsun mu?
+//         select: true, //when we call data, the selected data come too
+//         validate: [function(data){return true}, 'Error-Message'], //veriyi kontrolden geçiren fonksiyon
+//         enum: [['0','1','2','3'], 'Error-Message'], //pattern-choices -> buradaki değerlerden biri girilmeli.
+//         get: function(data){return data}, //data çağırılırken çalışan fonksiyon
+//         set: function(data){return data}, //datayı kaydederken çalışan fonksiyon
+//     }
+//     },
+//     {
+//         collection: 'collectionName',
+//         timestamps: true, //takip edilmesi için
+//     }
+//   );
+
+const blogPostSchema = new mongoose.Schema({
+    // _id (auto generated)
+    title: {
+        type: String,
+        trim: true,
         required: true,
-        required: [true, 'Error-message'], // error-message specification
-        trim: true, // get data with trim
-        unique: true,
-        index: true, // arama yapılınca daha hızlı erişim olsun mu?
-        select: true, //when we call data, the selected data come too
-        validate: [function(data){return true}, 'Error-Message'], //veriyi kontrolden geçiren fonksiyon
-        enum: [['0','1','2','3'], 'Error-Message'], //pattern-choices -> buradaki değerlerden biri girilmeli.
-        get: function(data){return data}, //data çağırılırken çalışan fonksiyon
-        set: function(data){return data}, //datayı kaydederken çalışan fonksiyon
-    }
     },
+    content: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    published: {
+        type: Boolean,
+        default: true,
+    },
+    // createdAt: {
+        
+    // },
+    // updatedAt: {
+        
+    // },
+    }, 
     {
-        collection: 'collectionName',
-        timestamps: true, //takip edilmesi için
-    }
-  );
+        collection: 'blogsPosts',
+        timestamps: true,
+    })
