3ff7af9b65828bee13f2dfd139269b1f8eb1d3d3
tarti <tartilaci@gmail.com>
2024-06-14 12:13:10 +0200

    "Updated blogController.js and blogRoute.js: added error handling, modified read and update methods, added delete and createMany methods, and updated route definitions."

--- BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/controllers/blogController.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/controllers/blogController.js
@@ -1,9 +1,10 @@
 //*Blog Controller
+require("express-async-errors"); //* hata fırlatmak
 const { BlogPost } = require("../models/blogModel");
 
 module.exports.BlogPostController = {
   list: async (req, res) => {
-    const data = await BlogPost.find({published: true});  // published true olanları getir!
+    const data = await BlogPost.find({ published: true });
 
     res.status(200).send({
       error: false,
@@ -18,21 +19,59 @@ module.exports.BlogPostController = {
       blog: data,
     });
   },
-
-  read:  async (req, res) => {
-    const data = await BlogPost.findById(req.params.id);  
+  read: async (req, res) => {
+    // const data = await BlogPost.findById(req.params.id); //* sadce id secenegini kabul eder.
+    // const data = await BlogPost.findOne({published: false });
+    const data = await BlogPost.findOne({ _id: req.params.id }); //* diğer seçenekleri de kabul eder.
 
     res.status(200).send({
       error: false,
       blog: data,
     });
   },
-
   update: async (req, res) => {
-    // const data = await BlogPost.findByIdAndUpdate(req.params.id, req.body); //! database'de günceller ama ui da eski veri görünür. onun da güncellenmesi için aşağıdaki gibi true eklenmeli:
-    const data = await BlogPost.findByIdAndUpdate(req.params.id, req.body, {new:true});
+    // const data = await BlogPost.findByIdAndUpdate(req.params.id,req.body,{new:true}) // {new:true} => return new data
+    const data = await BlogPost.updateOne({ _id: req.params.id }, req.body); //* datayı döndürmez yaptığı işlemin özetini döner. O nedenle bu yöntemde newData şeklinde sorgu yazıp güncellenmiş halini gönderebiliriz
 
     res.status(202).send({
+      error: false,
+      blog: data,
+      newData: await BlogPost.findOne({ _id: req.params.id }),
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await BlogPost.findByIdAndDelete(req.params.id);
+    if (data) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  },
+
+  createMany: async (req, res) => {
+    const data = await BlogPost.insertMany(req.body.blogs); //* Çoklu veri create etmek için kullanılan yöntem
+    //* çoklu veri gönderilirken veriyi json formatında gönderiyoruz:
+    //     {
+    //         "blogs": [
+    //     {
+    //       "title": "Blog Title 7",
+    //       "content": "Blog Content 7",
+    //       "published": false
+    //     },
+    //     {
+    //       "title": "Blog Title 8",
+    //       "content": "Blog Content 8",
+    //       "published": false
+    //     },
+    //     {
+    //       "title": "Blog Title 9",
+    //       "content": "Blog Content 9",
+    //       "published": false
+    //     }
+    //   ]
+    // }
+    res.status(201).send({
       error: false,
       blog: data,
     });
--- BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/routes/blogRoute.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/routes/blogRoute.js
@@ -8,11 +8,13 @@ router
   .route("/post")
   .get(BlogPostController.list)
   .post(BlogPostController.create);
-
-  router
+router
   .route("/post/:id")
   .get(BlogPostController.read)
   .put(BlogPostController.update)
+  .delete(BlogPostController.delete)
 
-  
+
+router.route("/post/createMany").post(BlogPostController.createMany);
+
 module.exports = router;
