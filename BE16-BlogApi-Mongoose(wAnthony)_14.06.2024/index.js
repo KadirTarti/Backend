a9179808365f53b76714df6101cd18cf28e34f12
tarti <tartilaci@gmail.com>
2024-06-14 11:04:53 +0200

    "Added blog route and controller, updated index.js to use new route"

--- BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/index.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/index.js
@@ -15,6 +15,9 @@ app.all('/', (req, res) => {
     res.send("<h1 style='text-align:center;margin-top:150px'>WELCOME TO BLOG API</h1>");
 })
 
+// Route
+app.use('blog', require('./src/routes/blogRoute'))
+
 // errorHandler:
 app.use(require('./src/middlewares/errorHandler'))
 
--- BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/routes/blogRoute.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14.06.2024/src/routes/blogRoute.js
@@ -1,1 +1,11 @@
 //* Blog Route
+
+const router = require('express').Router()
+
+const {BlogPostController} = require('../controllers/blogController')
+
+router.route('/post')
+    .get(BlogPostController.list)
+    .post(BlogPostController.create)
+
+
