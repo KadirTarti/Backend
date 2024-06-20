27a7d30c98a8e63ee3626076b5fc8a115149172e
tarti <tartilaci@gmail.com>
2024-06-20 17:20:34 +0200

    "Added url module and parsed URL in route handler; updated conditional statement to check pathname and method"

--- Exercises/assignment/app.js
+++ Exercises/assignment/app.js
@@ -2,7 +2,7 @@
 
 //http'yi node.js ortamında kullan.
 const http=require('node:http')   
-
+const url = require('url')
 
 //^(npm i http ile modülü yüklersek, sadece http yazarak çağırabiliyoruz)
 //^ const http=require('http')
@@ -11,9 +11,14 @@ const http=require('node:http')
 const routeHandler = (req, res) => {
     console.log(req.url)
     console.log(req.method)
-if (req.url == '/') {
+    const parsedUrl = url.parse(req.url, true)
+    console.log(parsedUrl)
+if (parsedUrl.pathname == '/' && req.method == 'GET') {
     res.write('Welcome to MyPage');
     res.end()
+    // if (req.url == '/') {
+//     res.write('Welcome to MyPage');
+//     res.end()
 } else {
     res.write('Page can not found');
     res.end()
