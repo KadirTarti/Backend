51ddce756cefa47aaf6a45e8365066e2c96e2d16
tarti <tartilaci@gmail.com>
2024-06-22 22:07:28 +0200

    "Added routes for book API endpoints"

--- Exercises/assignment1_TodoAPP/src/routes/bookRoute.js
+++ Exercises/assignment1_TodoAPP/src/routes/bookRoute.js
@@ -3,3 +3,22 @@
 const router = require('express').Router()
 
 const { BookPostController } = require('../controllers/bookController')
+
+router
+    .route('/post')
+    .get(BookPostController.list)
+    .post(BookPostController.create);
+
+router
+    .route('/post/many')
+    .post(BookPostController.createMany)
+    .delete(BookPostController.deleteMany);
+
+router
+    .route('/post/:id')
+    .get(BookPostController.read)
+    .put(BookPostController.update)
+    .delete(BookPostController.delete)
+
+    
+module.exports = router;
