2606febaf6cb158911b28c60918d964c4a0af150
tarti <tartilaci@gmail.com>
2024-06-22 22:26:41 +0200

    "Replaced mongoose with Sequelize, added support for SQLite and PostgreSQL databases, and introduced environment variables for database configuration."

--- Exercises/assignment1_TodoAPP/src/configs/connectDB.js
+++ Exercises/assignment1_TodoAPP/src/configs/connectDB.js
@@ -1,8 +1,31 @@
 //* MongoDb connection
 
-const mongoose = require('mongoose');
 
-mongoose
-.connect('mongodb://localhost:27017/bookAPI')
-.then(()=>console.log('DB Connection'))
-.catch((err) => console.log(err));
+const { Sequelize, DataTypes } = require("sequelize");
+
+const HOST=process.env?.HOST || '127.0.0.1'
+
+require("dotenv").config();
+const PSW = process.env?.PSW;
+const DB = process.env.DB;
+let sequelize;
+
+if (DB == "SQLITE") {
+  //SQLITE
+  sequelize = new Sequelize("sqlite:./datab.sqlite3");
+} else {
+  //POSTGRE
+  // npm install --save pg pg-hstore
+  // const sequelize= new Sequelize (`postgres://abdulkadir1:${PSW}@localhost:5432/todoapp`)
+
+  sequelize = new Sequelize(
+    `postgres://abdulkadir2:${PSW}@${HOST}:5432/todoapp`
+  );
+}
+
+sequelize
+  .authenticate() // connect to db
+  .then(() => console.log("BooKAPI DB connected"))
+  .catch(() => console.log("BooKAPI DB NOT connected"));
+
+module.exports = { sequelize, DataTypes };
