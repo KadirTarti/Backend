5ac33af02cc00ffc9b9725c9bf8b50d6429d71be
tarti <tartilaci@gmail.com>
2024-06-22 22:11:10 +0200

    "Switched from Sequelize to Mongoose for database connection and added error handler middleware"

--- Exercises/assignment1_TodoAPP/src/configs/connectDB.js
+++ Exercises/assignment1_TodoAPP/src/configs/connectDB.js
@@ -1,37 +1,8 @@
-//? express to DB connection
-//https://sequelize.org/docs/v6/getting-started/
+//* MongoDb connection
 
-const { Sequelize, DataTypes } = require("sequelize");
-//const sequelize=new Sequelize('RDB_name:adress')
+const mongoose = require('mongoose');
 
-const HOST=process.env?.HOST || '127.0.0.1'
-
-require("dotenv").config();
-const PSW = process.env?.PSW;
-const DB = process.env.DB;
-let sequelize;
-
-if (DB == "SQLITE") {
-    //SQLITE
-    sequelize = new Sequelize("sqlite:./db.sqlite3");
-  } else {
-    //POSTGRE
-    // npm install --save pg pg-hstore
-    // const sequelize= new Sequelize (`postgres://abdulkadir1:${PSW}@localhost:5432/todoapp`)
-  
-    sequelize = new Sequelize(
-      `postgres://abdulkadir1:${PSW}@${HOST}:5432/todoapp`
-    );
-  }
-
-
-  sequelize
-  .authenticate() // connect to db
-  .then(() => console.log("Book DB connected"))
-  .catch(() => console.log("Book DB NOT connected"));
-
-module.exports = { sequelize, DataTypes };
-
-
-
-module.exports= Book
+mongoose
+.connect('mongodb://localhost:27017/blogAPI')
+.then(()=>console.log('DB Connection'))
+.catch((err) => console.log(err));
