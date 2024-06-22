1ae8998d407ee6e7c963571e068575189af3f1d4
tarti <tartilaci@gmail.com>
2024-06-22 22:18:04 +0200

    "Refactored index.js: rearranged code, updated port and host variables, and modified app.listen logging"

--- Exercises/assignment1_TodoAPP/index.js
+++ Exercises/assignment1_TodoAPP/index.js
@@ -7,8 +7,12 @@ const app=express()
 require("express-async-errors")
 
 require('dotenv').config()
-const PORT=process.env?.PORT ||8000
-const HOST=process.env?.HOST ||'127.0.0.1'
+const PORT = process.env.PORT;
+
+//! json router'dan önce gelmeli. YERİ ÖNEMLİ
+// json to obj  and obj to json 
+app.use(express.json())
+
 
 // app.all('/', (req,res)=>{   // all sadece verilen pathe kontrol yapar
 //     res.send('Welcome to my API - TODO APP')
@@ -18,12 +22,9 @@ app.use('/', (req,res)=>{   // use verilen pathe eklenen şeyleri de kontrol ede
     res.send('Welcome to my API - TODO APP')
 })
 
-//! json router'dan önce gelmeli. YERİ ÖNEMLİ
-// json to obj  and obj to json 
-app.use(express.json())
 
-app.use(require('./src/routes/bookRoute'))
+app.use('/book', require('./src/routes/bookRoute'))
 
 app.use(require('./src/middlewares/errorHandler') )
 
-app.listen(PORT,()=>console.log('server runned on localhost') )
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
