d88460d2e4ebf7181a6951638f1ee73b45e7dd7f
tarti <tartilaci@gmail.com>
2024-06-22 21:51:02 +0200

    "Updated bookModel.js to define schema with explicit field types and validation, and added module exports."

--- Exercises/assignment1_TodoAPP/src/models/bookModel.js
+++ Exercises/assignment1_TodoAPP/src/models/bookModel.js
@@ -4,14 +4,49 @@ const mongoose = require('mongoose')
 
 const bookPostSchema = new mongoose.Schema(
     {
-        title,
-        author,
-        ISBN,
-        genre,
-        publicationYear,
-        image,
-        createdAt,
-        updatedAt,
-        
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        author: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        ISBN : {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        genre: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        publicationYear: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String, // Assuming this will store the URL of the image
+            trim: true,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+
     }
+
 )
+
+
+module.exports = {
+    BookPost: mongoose.model("BookPost", bookPostSchema),
+  };
