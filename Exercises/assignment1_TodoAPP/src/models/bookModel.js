c560086e3de20aed5458a2ad2a80ba705fe61c7a
tarti <tartilaci@gmail.com>
2024-06-22 21:45:13 +0200

    "Updated package-lock.json, package.json, and bookModel.js files"

--- Exercises/assignment1_TodoAPP/src/models/bookModel.js
+++ Exercises/assignment1_TodoAPP/src/models/bookModel.js
@@ -1,46 +1,17 @@
-//? create MODEL
-// const Todo=sequelize.define('table / model name',{'model details'})
+// Book Models
 
+const mongoose = require('mongoose')
 
-const {sequelize,DataTypes}=require('../configs/connectDB')
-const Book=sequelize.define('Books',{
-
-    
-    
-    // fieldName:{
-    //     type:DataTypes.BIGINT,
-    //     primaryKey:true,    // deafult false
-    //     unique:true,        // default false
-    //     autoIncrement:true, // default false
-    //     allowNull:false,    // default true 
-    //     comment:'my comment',
-    //     field:'custom name',
-    //     defaultValue:'default value'
-    // }
-    
-    //id:{}   // id field auto generated
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false
-
-    },
-    description: DataTypes.TEXT,  // just type 
-    // description:{
-    //     type:DataTypes.TEXT,  
-    //     },
-    priority:{ 
-        // type:DataTypes.TINYINT,
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        defaultValue:0 // -1: low, 0: Normal, 1 High
-    },
-
-    isDone:{
-        type:DataTypes.BOOLEAN,
-        defaultValue:false,
-        // allowNull:false
+const bookPostSchema = new mongoose.Schema(
+    {
+        title,
+        author,
+        ISBN,
+        genre,
+        publicationYear,
+        image,
+        createdAt,
+        updatedAt,
+        
     }
-    // createdDate & updatedDate auto generated
-    // createdDate:{ type:DataTypes.DATE }
-    // updateddDate:{ type:DataTypes.DATE }
-})
+)
