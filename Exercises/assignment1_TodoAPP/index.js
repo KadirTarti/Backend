3e046da08d88d7175cc9f78b84b3ad759d9f2abc
tarti <tartilaci@gmail.com>
2024-06-23 16:03:38 +0200

    "Switched from mongoose to pg, updated database connection and added cors middleware"

--- Exercises/assignment1_TodoAPP/index.js
+++ Exercises/assignment1_TodoAPP/index.js
@@ -3,18 +3,26 @@
 const express=require('express')
 const app=express()
 
-const mongoose = require('mongoose')
+const { Pool } = require('pg');
+const pool = new Pool({
+     port:process.env?.PORT ||  8000,
+     host:process.env?.HOST || '127.0.0.1',
+     password: process.env?.PSW,
+     database: process.env?.DB
+})
 
 //catch async-error  
 require("express-async-errors")
 
 require('dotenv').config()
-const PORT=process.env?.PORT ||  8000
-const HOST=process.env?.HOST || '127.0.0.1'
+
+
+const PORT = process.env?.PORT || 8000
 
 //! json router'dan önce gelmeli. YERİ ÖNEMLİ
 // json to obj  and obj to json 
-app.use(express.json())
+// app.use(express.json())
+app.use(cors());
 
 
 // app.all('/', (req,res)=>{   // all sadece verilen pathe kontrol yapar
