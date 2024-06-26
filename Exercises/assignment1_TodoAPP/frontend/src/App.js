6c5b82c38b5218e2d6435c631da1cf99bcbc0457
tarti <tartilaci@gmail.com>
2024-06-26 12:22:52 +0200

    modified

--- Exercises/assignment1_TodoAPP/frontend/src/App.js
+++ Exercises/assignment1_TodoAPP/frontend/src/App.js
@@ -1,46 +1,45 @@
-import React, {Fragment, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-  const [backendData, setBackendData] = useState([])
+  const url = "http://localhost:8000/books";
 
-  const getBooks = async () => {
-    try{
-      const response = await fetch ('http://localhost:8000/books')
-      const jsonData = await response.json();
-      setBackendData(jsonData)
-    } catch (err) {
-      console.log(err.message)
+  const [bookData, setBookData] = useState([])
+
+  const fetchBookData = () => {
+    axios.get(url).then((response) => {
+      setBookData(response.data)
+    }).catch((error) => {
+      console.log('error fetching book data:', error)})
     }
-  }
   
-  useEffect (()=>{
-        getBooks();
-      }, [])
-
-
+    console.log(bookData)
+    console.log(setBookData)
+  useEffect (()=>{  fetchBookData()}, [])
+  
+  
   return (
-    <form>
-    <h1>WELCOME TO MY BOOK API</h1>
-      <table class="table">
-<thead>
-  <tr>
-    <th>Description</th>
-    <th>Edit</th>
-    <th>Delete</th>
-  </tr>
-</thead>
-<tbody>
-  {backendData.map(book => (
-    <tr>
-        <td>{book.title}</td>
-        <td>Edit</td>
-        <td>Delete</td>
-    </tr>
-  ))}
-</tbody>
-</table>
-    </form>
+    <main>
+    <div>
+      <h1>Books</h1>
+    </div>
+      <div>
+        {bookData.length > 0 && bookData[0].post.title}
+      </div>
+      <div>
+        {bookData.length > 0 && bookData.post[0].author}
+      </div>
+      <div>
+        {bookData.length > 0 && bookData[0].ISBN}
+      </div>
+      <div>
+        {bookData.length > 0 && bookData[0].genre}
+      </div>
+      <div>
+        {bookData.length > 0 && bookData[0].publicationYear}
+      </div>
+    </main>
   )
 }
 
