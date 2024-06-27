a47dda7991f397efcbb78146531337a47a6e8b57
tarti <tartilaci@gmail.com>
2024-06-27 11:01:26 +0200

    "Added filtering, sorting, and pagination logic to BlogPostController"

--- BE16-BlogApi-Mongoose(wAnthony)_14+24+25+26+28.06.2024/src/controllers/blogController.js
+++ BE16-BlogApi-Mongoose(wAnthony)_14+24+25+26+28.06.2024/src/controllers/blogController.js
@@ -54,7 +54,9 @@ module.exports.BlogCategoryController = {
 module.exports.BlogPostController = {
   list: async (req, res) => {
   //! Filtering
+  // URL?filter[key1]=value1&filter[key2]=value2 => url array parameter
     // console.log(req.query)
+  
     const filter = req.query?.filter ||{}
     console.log('filter:', filter)
     
@@ -65,7 +67,7 @@ module.exports.BlogPostController = {
 
     const search = req.query?.search ||{}
     console.log('search:', search)
-    // {title: 'Testuser1', content: 'Testuser' } => {title: {$regex: 'Tstuser1'} , content {$regex: 'Testuser'} }
+    //* {title: 'Testuser1', content: 'Testuser' } => {title: {$regex: 'Tstuser1'} , content {$regex: 'Testuser'} }
 
     //& for döngüsünü kullanma nedenimiz kaç kelime ile search sorgusu yapılacağını bilmediğimizden
 
@@ -77,6 +79,10 @@ module.exports.BlogPostController = {
     console.log('search2:' , search)
 
     //! sorting
+    //https://mongoosejs.com/docs/api/query.html#Query.prototype.sort()
+    // URL?sort[key1]=value1&sort[key2]=value2 => url array parameter
+    // 1:A-Z - -1:Z-A deprecated
+    // asc:A-Z - desc:Z-A
     const sort = req.rquery?.sort ||{}
 
     //! pagination 
@@ -90,14 +96,19 @@ module.exports.BlogPostController = {
 
     //& page
     let page = Number(req.query?.page)
-    page = page > 0 ? (page-1) : 0
+    page = page > 0 ? (page-1) : 0  // Backend 'de sayfa sayısı her zmaan page-1 olarak hesaplanmalı. Kullanıcı yine page=1 olarak görecek ama hesaplama yapılruıken page-1 olarak hesaplama yapacağız. skip metodundan dolayı bunu yapıyoruz
     console.log(typeof page, page)
 
     //! skip  => atlanacak veri sayısı
     let skip = Number(req.query?.skip)
     skip = skip > 0 ? skip : (page*limit)
     console.log(typeof skip, skip)
-
+   // const data = await BlogPost.find({}) = BlogPost.find()
+    // const data = await BlogPost.find(filter);
+    // const data = await BlogPost.find({filter,search}); => {filter:{ userId: '667d10dc03839026052691ab', published: '0' },search:{ title: { '$regex': 'Testuser1' }, content: { '$regex': 'Testuser' } }} // wrong
+    // const [a,b,...x] = [12,13,56,6455,456] => rest => toplama
+    // function(a,...x) => rest
+    // const data = await BlogPost.find({ ...filter, ...search }); // spread => yayma
 
 
     const data = await BlogPost.find({...filter, ...search}).sort(sort).limit(limit).skip(skip)
