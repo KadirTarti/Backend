ee147731a8a5fc458e9c72fb19a1a6dbe4dac94c
tarti <tartilaci@gmail.com>
2024-06-28 09:49:27 +0200

    "Added mongoose schema and model for Tutorial"

--- BE17-Deployment-Tutorialapp_FE_&_BE/backend/models/tutorialModel.js
+++ BE17-Deployment-Tutorialapp_FE_&_BE/backend/models/tutorialModel.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
+
+const tutorialSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    collection:"tutorial",
+    timestamps:false
+  }
+);
+
+const Tutorial = mongoose.model("Tutorial",tutorialSchema)
+
+module.exports = Tutorial;
