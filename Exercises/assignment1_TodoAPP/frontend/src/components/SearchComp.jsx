7de5d67bfe983f1d5e68494a49682b0630d22a70
tarti <tartilaci@gmail.com>
2024-06-29 22:17:54 +0200

    "Updated App.css to add .magn-icon class and modified SearchComp.jsx to use .magn-icon class for svg element"

--- Exercises/assignment1_TodoAPP/frontend/src/components/SearchComp.jsx
+++ Exercises/assignment1_TodoAPP/frontend/src/components/SearchComp.jsx
@@ -17,7 +17,7 @@ const SearchComp: React.FC<ISearchComp> = ({ handleChange }) => {
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg
                         aria-hidden="true"
-                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        className="magn-icon w-5 h-5 text-gray-500 dark:text-gray-400"
                         fill="none"
                         stroke="currentColor"
                         viewBox="0 0 24 24"
