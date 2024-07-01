e1a4e88aad9dc9af0840215ab312ea0d737e0ea4
tarti <tartilaci@gmail.com>
2024-07-01 13:21:40 +0200

    "Added new route for getting personnels by department ID and added patch method to update department"

--- BE18-PersonnelApi/src/routes/department.router.js
+++ BE18-PersonnelApi/src/routes/department.router.js
@@ -8,14 +8,18 @@ const router = require("express").Router();
 const department = require("../controllers/department.controller");
 const idValidation = require("../middlewares/idValidation");
 
-//* url:  /departments
+//* URL : /departments
+
 router.route("/").get(department.list).post(department.create);
+
+router.route("/:id/personnels").get(department.personnels);//* departmana göre personel listeleme
 
 router
   .route("/:id")
   .all(idValidation)
   .get(department.read)
   .put(department.update)
+  .patch(department.update)
   .delete(department.delete);
 
 /* ------------------------------------------------------- */
