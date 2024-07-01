3b1c5caa46e6e86c8fc211161e606eb9b6926b20
tarti <tartilaci@gmail.com>
2024-07-01 11:14:05 +0200

    "Added personnel controller and id validation middleware to personnel router, and defined routes for CRUD operations"

--- BE18-PersonnelApi/src/routes/personnel.router.js
+++ BE18-PersonnelApi/src/routes/personnel.router.js
@@ -5,7 +5,21 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const personnel = require("../controllers/personnel.controller");
+const idValidation = require("../middlewares/idValidation");
+
+//* url:  /personnels
+router.route("/").get(personnel.list).post(personnel.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(personnel.read)
+  .put(personnel.update)
+  .patch(personnel.update)
+  .delete(personnel.delete);
 
 
-/* ------------------------------------------------------- */
-module.exports = router
+
+  /* ------------------------------------------------------- */
+module.exports = router;
