53d42c681c9c608d7c79f3089f06718586a5e7d5
tarti <tartilaci@gmail.com>
2024-07-01 09:27:47 +0200

    "Refactored index.js: reorganized code, added error handling, database connection, and middleware configurations"

--- BE18-PersonnelApi/index.js
+++ BE18-PersonnelApi/index.js
@@ -13,14 +13,46 @@ const app = express()
 
 
 
-//env to process.env
+//* envVariables to process.env
 require('dotenv').config()
-
-
 const PORT = process.env.PORT || 8000
 
 
+/* -------------------------------------------------------------------------- */
+/*                                errorHandler                                */
+/* -------------------------------------------------------------------------- */
+//& asyncErrors to errorHandler
+require('express-async-errors')
 
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                               configurations                               */
+/* -------------------------------------------------------------------------- */
+//! db connection
+const {dbConnection} = require('./src/configs/dbConnection')
+const errorHandler = require('./src/middlewares/errorHandler')
+dbConnection()
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 Middlewares                                */
+/* -------------------------------------------------------------------------- */
+//* accept json
+app.use(express.json())
+
+//* filter, search, sort, pegination(res.getModelList)
+require('./src/middlewares/findSearchSortPagi')
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   routes                                   */
+/* -------------------------------------------------------------------------- */
+app.all("/",(req,res)=> {
+    res.send("Welcome to the Personnel API")
+})
 
 
 /* ------------------------------------------------------- */
