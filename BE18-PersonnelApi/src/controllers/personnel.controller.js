0c08aa8b8cacdd4720c03379935b38bdd105bf2b
tarti <tartilaci@gmail.com>
2024-07-01 11:09:51 +0200

    "Formatted personnel controller code, added/removed whitespace, and fixed syntax errors"

--- BE18-PersonnelApi/src/controllers/personnel.controller.js
+++ BE18-PersonnelApi/src/controllers/personnel.controller.js
@@ -1,61 +1,73 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-
-const Personnel = require('../models/personnel.model')
+const Personnel = require("../models/personnel.model");
 
 module.exports = {
-    list:async(req, res)=> {
-        // const personnels = await res.getModelList (personnel)
-        const data = await res.getModelList(Personnel)
-        res.status(200).send({
-            error:false,
-            detail: await res.getModelListDetails(personnel),
-           // personnels
-            data
-        })
-    },
+  list: async (req, res) => {
+    const data = await res.getModelList(Personnel);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Personnel),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const isLead = req.body?.isLead || false;
+    if (isLead) {
+      await Personnel.updateMany(
+        {
+          departmentId: req.body.departmentId,
+          isLead: true,
+        },
+        { isLead: false }
+      );
+    }
 
-    create:async(req, res)=> {
-        const isLead  = req.body?.isLead ||false,
-        if(isLead) {
-            await Personnel.updateMany({
-                departmentId:req.nody.departmentId, isLead:true
-            }, {isLead:false})
-        }
-        const data =     await Personnel.create(req.body)
-        res.status(201).send({
-            error:false,
-            data
-        })
-    },
+    const data = await Personnel.create(req.body);
 
-    read:async(req, res)=> {
-        const data = await Personnel.findById({_id: req.params.id})
-        res.status(200).send({
-            error:false,
-            data
-        })
-    },
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Personnel.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const isLead = req.body?.isLead || false;
+    if (isLead) {
+      const { departmentId } = await Personnel.findOne({ _id: req.params.id });
+      console.log(departmentId);
+      await Personnel.updateMany(
+        {
+          departmentId,
+          isLead: true,
+        },
+        { isLead: false }
+      );
+    }
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true, //* modelde var olan validate fonksiyonlarının(built-in ve custom) update işlemi sırasında çalışmasını sağlayan özellik ***
+    });
+    res.status(202).send({
+      error: false,
+      data,
+      newData: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Personnel.deleteOne({ _id: req.params.id });
 
-    update:async(req, res)=> {
-        const data = await Personnel.updateOne({_id: req.paramsid}, req.body, {
-            runValidators: true
-        });
-        res.status(202).send({
-            error: false,
-            data,
-            newData: await Personnel.findOne({ _id: req.params.id})
-        })
-    },
-
-    delete:async(req, res)=> {
-        const data = await Personnel.deleteOne ({_id: rq.params.id})
-        res.status(data.deletedCount > 0 ? 204 : 404).send({
-            error: !data.deletedCount,
-            data
-        })
-    },
-}
+    res.status(data.deletedCount > 0 ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
