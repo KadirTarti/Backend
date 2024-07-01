023ffbae50bbfa85575cf429cfbf889d8786c910
tarti <tartilaci@gmail.com>
2024-07-01 09:35:23 +0200

    "Implemented list, create, and read functions in department controller"

--- BE18-PersonnelApi/src/controllers/department.controller.js
+++ BE18-PersonnelApi/src/controllers/department.controller.js
@@ -6,9 +6,32 @@
 const Department = require('../models/department.model')
 
 module.exports = {
-    list:async(req, res)=> {},
-    create:async(req, res)=> {},
-    read:async(req, res)=> {},
+    list:async(req, res)=> {
+        // const departments = await res.getModelList (Department)
+        const data = await res.getModelList (Department)
+        res.status(200).send({
+            error:false,
+            detail: await res.getModelListDetails(Department),
+           // departments
+            data
+        })
+    },
+
+    create:async(req, res)=> {
+        const data = await Department.create(req.body)
+        res.status(201).send({
+            error:false,
+            data
+        })
+    },
+    
+    read:async(req, res)=> {
+        const data = await Department.findById(req.params.id)
+        res.status(200).send({
+            error:false,
+            data
+        })
+    },
     update:async(req, res)=> {},
     delete:async(req, res)=> {},
 }
