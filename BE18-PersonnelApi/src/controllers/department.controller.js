657b542edba1fa1d2ec82587695c9874450a0ebf
tarti <tartilaci@gmail.com>
2024-07-01 09:30:49 +0200

    "Added Department model import and empty controller functions to department.controller.js"

--- BE18-PersonnelApi/src/controllers/department.controller.js
+++ BE18-PersonnelApi/src/controllers/department.controller.js
@@ -2,3 +2,13 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require('../models/department.model')
+
+module.exports = {
+    list:async(req, res)=> {},
+    create:async(req, res)=> {},
+    read:async(req, res)=> {},
+    update:async(req, res)=> {},
+    delete:async(req, res)=> {},
+}
