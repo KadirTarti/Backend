a121f323f9588cf91af9d198d2c6213098c66b35
tarti <tartilaci@gmail.com>
2024-07-01 10:37:54 +0200

    "Added Personnel model schema and middleware for ID validation in department router"

--- BE18-PersonnelApi/src/models/personnel.model.js
+++ BE18-PersonnelApi/src/models/personnel.model.js
@@ -4,3 +4,50 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const PersonelSchema = new mongoose.Schema({
+    departmentId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Department',
+        required: true
+    },
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true    // username ile de sisteme giriş yapması için unique denilebilir
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set:(password)=> passwordEncrypt(password)
+    },
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: ttrue,
+        validate: (email) => email.includes('@') && email.includes()
+    }
+
+}, 
+{
+    collection: 'personel',
+    timestamps: true
+})
--- BE18-PersonnelApi/src/routes/department.router.js
+++ BE18-PersonnelApi/src/routes/department.router.js
@@ -6,12 +6,14 @@ const router = require("express").Router();
 /* ------------------------------------------------------- */
 
 const department = require("../controllers/department.controller");
+const idValidation = require("../middlewares/idValidation");
 
 //* url:  /departments
 router.route("/").get(department.list).post(department.create);
 
 router
   .route("/:id")
+  .all(idValidation)
   .get(department.read)
   .put(department.update)
   .delete(department.delete);
