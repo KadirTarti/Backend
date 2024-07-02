14b4c4a293fddaa0b9f40099e6e2e2645143a122
tarti <tartilaci@gmail.com>
2024-07-02 11:11:02 +0200

    add authentication.js

--- BE18-PersonnelApi/src/middlewares/authentication.js
+++ BE18-PersonnelApi/src/middlewares/authentication.js
@@ -2,7 +2,12 @@
 
 
 const Token = require('../models/token.model')
+//^token bilgisi standart olarak headers başlığında gönderilir
+/*
+expressjs te gelen req üzerinden headersı yakalayabiliyoruz. console'dan görebiliyoruz. token bilgisi headers authorization ile gönderilir. Thunder'da headers başlığına authorization ekledik Token (boşluk) 
+*/
+
 
 module.exports = async(req,res,next) => {
-    
+
 }
