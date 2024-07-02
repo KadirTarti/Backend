4942c68458c46ba63c15af4e22eb446ab77b690e
tarti <tartilaci@gmail.com>
2024-07-02 10:32:09 +0200

    "Added auth router and removed trailing newline"

--- BE18-PersonnelApi/src/routes/index.js
+++ BE18-PersonnelApi/src/routes/index.js
@@ -10,12 +10,13 @@ const router = require("express").Router();
 
 //! ana index.js'teki tüm route'ları burada topladık, orayı rahatlatmak için !
 
+router.use('/auth', require('./auth.router'))
+
 router.use("/departments", require("./department.router"));
 
 router.use("/personnels", require("./personnel.router"));
 
 router.use('/tokens', require('./token.router'))
-
 
 
 module.exports= router
