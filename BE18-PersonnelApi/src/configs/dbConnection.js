deba23b70ea5d9d44ee73d24b65270e6d5040d27
tarti <tartilaci@gmail.com>
2024-07-04 10:16:09 +0200

    "Updated MongoDB connection string in dbConnection.js"

--- BE18-PersonnelApi/src/configs/dbConnection.js
+++ BE18-PersonnelApi/src/configs/dbConnection.js
@@ -10,7 +10,9 @@ const mongoose = require('mongoose')
 const dbConnection = function() {
     // Connect:
     console.log(process.env.MONGODB)
-    mongoose.connect('mongodb://127.0.0.1:27017/personnelAPI')
+    // mongoose.connect('mongodb://127.0.0.1:27017/personnelAPI')
+    // mongoose.connect('mongodb://127.0.0.1:27017/personnelAPI')
+    mongoose.connect('mongodb://abdulkadir:1234@localhost:27017/personelAPI?authSource=admin')
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
