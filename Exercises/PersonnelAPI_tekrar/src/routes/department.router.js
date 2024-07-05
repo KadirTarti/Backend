bbcd74efe32ef8ed9754971c4936a03f7eeaf9fb
tarti <tartilaci@gmail.com>
2024-07-05 13:58:36 +0200

    "Added department controller, id validation, and permission middlewares to department router"

--- Exercises/PersonnelAPI_tekrar/src/routes/department.router.js
+++ Exercises/PersonnelAPI_tekrar/src/routes/department.router.js
@@ -1,11 +1,26 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
+const department = require("../controllers/department.controller");
+const idValidation = require("../middlewares/idValidation");
+const permission = require('../middlewares/permissions')
 
+//* URL : /departments
+
+router.route("/").get(permission.isLogin, department.list).post(permission.isAdmin, department.create);
+
+router.route("/:id/personnels").get(permission.isAdminOrLead,department.personnels);//* departmana göre personel listeleme
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(permission.isLogin, department.read)
+  .put( permission.isAdmin, department.update)
+  .patch(permission.isAdmin, department.update)
+  .delete(permission.isAdmin, department.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
