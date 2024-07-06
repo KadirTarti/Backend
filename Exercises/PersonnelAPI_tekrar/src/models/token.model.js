ec1efed7fecc0ca2ef471045f24ceebfc93d7527
tarti <tartilaci@gmail.com>
2024-07-06 23:28:55 +0200

    add tokenmodel.js - create TokenSchema

---
+++ Exercises/PersonnelAPI_tekrar/src/models/token.model.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const { mongo } = require('mongoose')
+const {mongoose}= require('../configs/dbConnection')
+
+
+const TokenSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Personnel',
+        required: true,
+        index: true,
+        unique: true
+    },
+    token: {
+        type:String,
+        trim: true,
+        required: true,
+        index:true,
+        unique: true
+    }
+},{})
