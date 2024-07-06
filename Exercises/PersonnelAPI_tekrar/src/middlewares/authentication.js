cc4b1af2dca2bbf29a040aea82fa037f2afd703d
tarti <tartilaci@gmail.com>
2024-07-07 01:42:18 +0200

    "Uncommented authentication middleware code"

--- Exercises/PersonnelAPI_tekrar/src/middlewares/authentication.js
+++ Exercises/PersonnelAPI_tekrar/src/middlewares/authentication.js
@@ -22,11 +22,11 @@ console.log(auth);
 const tokenKey = auth ? auth.split(" ") : null
 console.log(tokenKey);
 
-// if(tokenKey && tokenKey[0] == 'Token'){
-//   const tokenData = await Token.findOne({ token: tokenKey[1] }).populate("userId");
-//   console.log(tokenData);
-//   if(tokenData) req.user = tokenData.userId
-// }
+if(tokenKey && tokenKey[0] == 'Token'){
+  const tokenData = await Token.findOne({ token: tokenKey[1] }).populate("userId");
+  console.log(tokenData);
+  if(tokenData) req.user = tokenData.userId
+}
 
 next();
 }
