f7f19344cf6b6000050a9ac05fa6f7973d5cc798
tarti <tartilaci@gmail.com>
2024-07-07 00:02:02 +0200

    add auth router

---
+++ Exercises/PersonnelAPI_tekrar/src/routes/auth.router.js
@@ -0,0 +1,20 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+
+const auth = require("../controllers/auth.controller")
+
+// URL => /auth
+
+// router.post("/login", auth.login)
+// // router.all("/logout", auth.logout) // swagger .all metodunu görmez
+// router.get("/logout", auth.logout)
+
+// // error handler, if no route has been caught
+// router.get("/*", function(req, res){ res.send("404 not found"); res.end();});
+// router.post("/*", function(req, res){ res.send("404 not found"); res.end();});
+
+module.exports = router
