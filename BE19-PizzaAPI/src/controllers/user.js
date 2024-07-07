cc750613163b2891096d2cbb316224cf6319f901
tarti <tartilaci@gmail.com>
2024-07-07 23:45:50 +0200

    "Added UserSchema and mongoose model to user.js controller"

--- BE19-PizzaAPI/src/controllers/user.js
+++ BE19-PizzaAPI/src/controllers/user.js
@@ -2,3 +2,45 @@
 /* -------------------------------------------------------
         ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
+
+const { mongoose } = require('../configs/dbConnection')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+/* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+    
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true    // username ile de sisteme giriş yapması için unique denilebilir
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set:(password)=> passwordEncrypt(password)
+
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        validate: (email) => email.includes('@') && email.includes()
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }, 
+}, 
+{
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Users', UserSchema)
