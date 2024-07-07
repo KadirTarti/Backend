b4da4b8381ff4158dbe5acd49e47b864cac1210c
tarti <tartilaci@gmail.com>
2024-07-07 20:10:47 +0200

    "Removed permission checks from token router and added isAdmin middleware to router"

--- Exercises/PersonnelAPI_tekrar/src/routes/token.router.js
+++ Exercises/PersonnelAPI_tekrar/src/routes/token.router.js
@@ -9,16 +9,32 @@ const token = require("../controllers/token.controller");
 const idValidation = require("../middlewares/idValidation");
 
 //* URL : /tokens 
+// const permission = require('../middlewares/permissions')
 
-router.route("/").get(permission.isAdmin, token.list).post(permission.isAdmin, token.create);
+// router.route("/").get(permission.isAdmin, token.list).post(permission.isAdmin, token.create);
+
+// router
+//   .route("/:id")
+//   .all(idValidation)
+//   .get(permission.isAdmin, token.read)
+//   .put(permission.isAdmin, token.update)
+//   .patch(permission.isAdmin, token.update)
+//   .delete(permission.isAdmin, token.delete);
+
+const {isAdmin} = require('../middlewares/permissions')
+
+router.use(isAdmin)
+
+router.route("/").get(token.list).post(token.create);
 
 router
   .route("/:id")
   .all(idValidation)
-  .get(permission.isAdmin, token.read)
-  .put(permission.isAdmin, token.update)
-  .patch(permission.isAdmin, token.update)
-  .delete(permission.isAdmin, token.delete);
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
+
 
 /* ------------------------------------------------------- */
 module.exports = router;
