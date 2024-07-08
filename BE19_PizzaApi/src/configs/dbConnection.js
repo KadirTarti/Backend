7f1fd45edc3579007157c0bf3a16a619149d4d5a
tarti <tartilaci@gmail.com>
2024-07-08 11:58:21 +0200

    "Updated MongoDB connection string in dbConnection.js"

--- BE19_PizzaApi/src/configs/dbConnection.js
+++ BE19_PizzaApi/src/configs/dbConnection.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = function() {
     // Connect:
-    mongoose.connect(process.env.MONGODB)
+    mongoose.connect('mongodb://127.0.0.1:27017/pizzaAPI')
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
