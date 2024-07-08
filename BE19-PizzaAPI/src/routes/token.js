3164705ac2171bf50d9ded2e5fb6008fec792778
tarti <tartilaci@gmail.com>
2024-07-08 10:40:43 +0200

    "Added token routes and middleware to token.js file"

--- BE19-PizzaAPI/src/routes/token.js
+++ BE19-PizzaAPI/src/routes/token.js
@@ -5,5 +5,21 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+
+
+const token = require("../controllers/token");
+const idValidation = require("../middlewares/idValidation");
+
+//* /tokens
+router.route("/").get(token.list).post(token.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(token.read)
+  .put(token.update)
+  .patch(token.update)
+  .delete(token.delete);
+
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
