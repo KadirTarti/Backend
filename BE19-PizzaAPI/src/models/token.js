df0a2a2d5578bbf3f71d421574835a3e4cc30b83
tarti <tartilaci@gmail.com>
2024-07-08 10:37:42 +0200

    "Added TokenSchema and Token model to token.js file"

--- BE19-PizzaAPI/src/models/token.js
+++ BE19-PizzaAPI/src/models/token.js
@@ -4,3 +4,27 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        trimm: true,
+        required: true,
+        index: true,
+        unique: true
+    }
+
+}, {collection: 'tokens',
+    timestamps: false
+})
+
+module.exports = mongoose.model('Token', TokenSchema)
