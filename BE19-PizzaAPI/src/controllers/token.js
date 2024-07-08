ab864acd4c3388cc32f3d4dc637d222f2449401b
tarti <tartilaci@gmail.com>
2024-07-08 10:39:11 +0200

    "Added Token controller with CRUD operations"

--- BE19-PizzaAPI/src/controllers/token.js
+++ BE19-PizzaAPI/src/controllers/token.js
@@ -2,3 +2,49 @@
 /* -------------------------------------------------------
         ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
+
+const Token = require("../models/token");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Token);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails,
+      data,
+    });
+  },
+  //! CRUD(Create-Read-Update-Delete)
+  create: async (req, res) => {
+    const data = await Token.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Token.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await Token.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    res.status(202).send({
+      error: false,
+      data,
+      newData: await Token.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Token.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+      message: "Token not found!",
+    });
+  },
+};
