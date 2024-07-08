4cf2cdda47136bdcec2ec4754a7a39cbc2f78903
tarti <tartilaci@gmail.com>
2024-07-08 19:52:05 +0200

    "Added PizzaSchema to pizza.js model with fields for name, image, price, and toppingIds, and exported the Pizza model."

--- BE19-PizzaAPI/src/models/pizza.js
+++ BE19-PizzaAPI/src/models/pizza.js
@@ -4,3 +4,39 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const PizzaSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    // toppingIds: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Topping",
+    // }, //* Many to One
+    toppingIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Topping",
+      },
+    ], //* Many to Many
+  },
+  {
+    collection: "pizzas",
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model("Pizza",PizzaSchema)
