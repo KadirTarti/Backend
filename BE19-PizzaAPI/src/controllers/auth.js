edf201797c49d49d16eb737ea83e95c03f098382
tarti <tartilaci@gmail.com>
2024-07-08 11:02:40 +0200

    "Refactored auth controller: updated login and logout functions, added token creation and sending in login response"

--- BE19-PizzaAPI/src/controllers/auth.js
+++ BE19-PizzaAPI/src/controllers/auth.js
@@ -1,36 +1,42 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
 
-const User = require('../models/user')
-const Token = require('../models/token');
-const { CustomError } = require('../errors/customError');
-const passwordEncrypt = require('../helpers/passwordEncrypt');
-
+const User = require("../models/user");
+const Token = require("../models/token");
+const { CustomError } = require("../errors/customError");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
 module.exports = {
-    login: async (req, res)=> {
-        const {username, email, password} = req.body;
-        if(password && (username || email)) {
-            // const user = await User.findOne({username,email}) //* bu şekilde username ve email birlikte arar. aşağıda or opr ile yazdık
-            const user = await User.findOne({$or:[{username},{email}]})
-            if (user && user.password == passwordEncrypt(password)) {
-                if(user.isActive){
-
-                }else {
-                    throw new CustomError('This account is inactive!', 401)
-                }
-
-            } else {
-                throw new Error ('Wrong username/email or password!')
-            }
+  login: async (req, res) => {
+    const { username, email, password } = req.body;
+    if (password && (username || email)) {
+      // const user = await User.findOne({username,email}) //* bu şekilde username ve email birlikte arar. aşağıda or opr ile yazdık
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+      if (user && user.password == passwordEncrypt(password)) {
+        if (user.isActive) {
+          let tokenData = await Token.findOne({ userId: user._id });
+          if (!tokenData) {
+            tokenData = await Token.create({
+              userId: user._id,
+              token: passwordEncrypt(user._id + Date.now()),
+            });
+          }
+          res.status(200).send({
+            error: false,
+            token: tokenData.token,
+            user,
+          });
         } else {
-            throw new CustomError ('Please enter username/email and password!', 401)
+          throw new CustomError("This account is inactive!", 401);
         }
-
-    },
-    logout: async (req, res)=> {
-
+      } else {
+        throw new Error("Wrong username/email or password!");
+      }
+    } else {
+      throw new CustomError("Please enter username/email and password!", 401);
     }
-}
+  },
+  logout: async (req, res) => {},
+};
