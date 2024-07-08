ccf1e90cc0fd5391af8242a244a3342053f48502
tarti <tartilaci@gmail.com>
2024-07-08 19:58:31 +0200

    "Added topping controller and middleware to topping route, defined CRUD operations"

--- BE19-PizzaAPI/src/routes/topping.js
+++ BE19-PizzaAPI/src/routes/topping.js
@@ -5,5 +5,18 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const topping = require("../controllers/topping");
+const idValidation = require("../middlewares/idValidation");
+
+//* /toppings
+router.route("/").get(topping.list).post(topping.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(topping.read)
+  .put(topping.update)
+  .patch(topping.update)
+  .delete(topping.delete);
 /* ------------------------------------------------------- */
 module.exports = router
