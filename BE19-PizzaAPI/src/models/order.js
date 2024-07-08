1abb791fad285d34b389599caa6cbedd116ecff4
tarti <tartilaci@gmail.com>
2024-07-08 19:51:39 +0200

    "Refactored Order model schema, updated data types and added validation, and experimented with different approaches for calculating 'amount' field."

--- BE19-PizzaAPI/src/models/order.js
+++ BE19-PizzaAPI/src/models/order.js
@@ -1,46 +1,133 @@
-// "use strict"
-// /* -------------------------------------------------------
-//     ABDULKADIR TARTILACI - EXPRESS - PIZZA API
-// ------------------------------------------------------- */
-// const { mongoose } = require('../configs/dbConnection')
+"use strict"
+/* -------------------------------------------------------
+    ABDULKADIR TARTILACI - EXPRESS - PIZZA API
+------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
 
-// /* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
 
-// const OrderSchema = new mongoose.Schema({
-//     UserId : {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User',
-//         required: true
+
+//* transform kullanımı  (amount)
+// const OrderSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
 //     },
-//     PizzaId : {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Pizza',
-//         required: true
+//     pizzaId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Pizza",
+//       required: true,
 //     },
 //     size: {
-//         type: String,
-//         trim: true,
-//         required: true,
+//       type: String,
+//       trim: true,
+//       required: true,
+//       enum: ["Small", "Medium", "Large", "Xlarge"],
 //     },
 //     quantity: {
-//         type: String,
-//         trim: true,
-//         required: true,
+//       type: Number,
+//       default: 1,
+//       validate: (quantity) => quantity > 0,
 //     },
 //     price: {
-//         type: String,
-//         trim: true,
-//         required: true,
+//       type: Number,
+//       required: true,
 //     },
 //     amount: {
-//         type: String,
-//         trim: true,
-//         required: true,
+//       type: Number,
+//       default: function () {
+//         return this.quantity * this.price;
+//       },//* create de problem yok sağlıklı çalışıyor
+//       transform: function () {
+//         return this.quantity * this.price;
+//       },//* dönüştürme işlemi gerçekleşirken çalışır
 //     },
-// }, 
-// {
-//     collection: 'order',
-//     timestamps: true
-// })
+//   },
+//   {
+//     collection: "orders",
+//     timestamps: true,
+//   }
+// );
 
-// module.exports = mongoose.model('Order', OrderSchema)
+//* getters kullanımı  (amount)
+// const OrderSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     pizzaId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Pizza",
+//       required: true,
+//     },
+//     size: {
+//       type: String,
+//       trim: true,
+//       required: true,
+//       enum: ["Small", "Medium", "Large", "Xlarge"],
+//     },
+//     quantity: {
+//       type: Number,
+//       default: 1,
+//       validate: (quantity) => quantity > 0,
+//     },
+//     price: {
+//       type: Number,
+//       required: true,
+//     },
+//     amount: {
+//       type: Number,
+//       get: function(){return this.quantity * this.price}
+//     },
+//   },
+//   {
+//     collection: "orders",
+//     timestamps: true,
+//     toJSON: { getters: true },
+//   }
+// );
+//! amount verisini db de saklamadan sadece ve sadece veri okuması yapılırken amount alanı hesaplanarak dönen veriye dinamik olarak eklensin. Bu yöntem de db de amount alanı olmayacak o nedenle amount a göre filtreleme yapamayız. Ve getters/transform her istekte çalışacağı için db de verinin fazla olduğu durumlarda performans sorunu yaşanabilir. Dikkatli kullanılması gereken bir metot.
+//* pre middleware
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    pizzaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pizza",
+      required: true,
+    },
+    size: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: ["Small", "Medium", "Large", "Xlarge"],
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+      validate: (quantity) => quantity > 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    amount: {
+      type: Number,
+    },
+  },
+  {
+    collection: "orders",
+    timestamps: true,
+  }
+);
+
+
+module.exports = mongoose.model("Order",OrderSchema)
