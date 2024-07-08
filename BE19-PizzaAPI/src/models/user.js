0c9b05c4957b90e8b6c380b272d194fc79c2ad0f
tarti <tartilaci@gmail.com>
2024-07-08 09:23:04 +0200

    "Refactor User model: rename schema and collection, remove departmentId, firstName, lastName, phone, title, salary, description, isAdmin, isLead, and startedAt fields, and update username and email fields."

--- BE19-PizzaAPI/src/models/user.js
+++ BE19-PizzaAPI/src/models/user.js
@@ -6,12 +6,7 @@ const { mongoose } = require('../configs/dbConnection')
 const passwordEncrypt = require('../helpers/passwordEncrypt')
 /* ------------------------------------------------------- */
 
-const PersonnelSchema = new mongoose.Schema({
-    departmentId : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Department',
-        required: true
-    },
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         trim: true,
@@ -24,21 +19,6 @@ const PersonnelSchema = new mongoose.Schema({
         required: true,
         set:(password)=> passwordEncrypt(password)
     },
-    firstName: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    phone: {
-        type: String,
-        trim: true,
-        required: true,
-    },
     email: {
         type: String,
         trim: true,
@@ -46,41 +26,15 @@ const PersonnelSchema = new mongoose.Schema({
         unique: true,
         validate: (email) => email.includes('@') && email.includes()
     },
-    title: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-
-    salary: {
-        type: Number,
-        default: 0,
-    },
-    description: {
-        type: String,
-        trim: true,
-        default: null,
-    },
+    
     isActive: {
         type: Boolean,
         default: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    }, 
-    isLead: {
-        type: Boolean,
-        default: false
-    },
-    startedAt: {
-        type: Date,
-        default: Date.now
-    }
 }, 
 {
-    collection: 'personnel',
+    collection: 'users',
     timestamps: true
 })
 
-module.exports = mongoose.model('Personnel', PersonnelSchema)
+module.exports = mongoose.model('User', UserSchema)
