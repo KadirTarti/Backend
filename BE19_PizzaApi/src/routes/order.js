3bbe0601c04777b1829c6e9e9e443da4e061bdd6
tarti <tartilaci@gmail.com>
2024-07-08 12:13:35 +0200

    "Added order controller and routes for CRUD operations"

--- BE19_PizzaApi/src/routes/order.js
+++ BE19_PizzaApi/src/routes/order.js
@@ -4,6 +4,19 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const order = require("../controllers/order");
+const idValidation = require("../middlewares/idValidation");
+
+//* /orders
+router.route("/").get(order.list).post(order.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(order.read)
+  .put(order.update)
+  .patch(order.update)
+  .delete(order.delete);
 
 /* ------------------------------------------------------- */
 module.exports = router
