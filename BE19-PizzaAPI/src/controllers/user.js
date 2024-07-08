b56b669671f8815b5f838245bbbd4b6309e6aaf9
tarti <tartilaci@gmail.com>
2024-07-08 09:38:21 +0200

    "Updated user controller with implemented CRUD operations"

--- BE19-PizzaAPI/src/controllers/user.js
+++ BE19-PizzaAPI/src/controllers/user.js
@@ -1,22 +1,46 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
         ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
-const User = require('../models/order')
+const User = require("../models/order");
 
 /* ------------------------------------------------------- */
-// create - read- update- delete isimlendirmesi şart değil. farklı başlıklar konabilir
 module.exports = {
-    list: async(req, res)=>{
-        const data = await res.getModelList(User)
-        res.status(200).send({
-            error: false,
-            details: await res.getModelListDetails,
-            data
-        })
-    },
-    create: async(req, res)=>{},
-    read: async(req, res)=>{},
-    update: async(req, res)=>{},
-    delete: async(req, res)=>{},
-}
+  list: async (req, res) => {
+    const data = await res.getModelList(User);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails,
+      data,
+    });
+  },
+  // create - read- update- delete isimlendirmesi şart değil. farklı başlıklar konabilir
+  create: async (req, res) => {
+    const data = await User.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await User.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await User.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await User.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204: 404).send({
+        error: !data.deletedCount,
+        data,
+        message: 'User not found!'
+        
+    })
+  },
+};
