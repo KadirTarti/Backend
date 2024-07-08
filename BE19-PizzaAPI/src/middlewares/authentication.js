366295aef91e9429c3b51f305ead706fdebdd9c1
tarti <tartilaci@gmail.com>
2024-07-08 11:41:47 +0200

    "Added authentication middleware to validate token and retrieve user data"

--- BE19-PizzaAPI/src/middlewares/authentication.js
+++ BE19-PizzaAPI/src/middlewares/authentication.js
@@ -2,3 +2,14 @@
 /* -------------------------------------------------------
         ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
+
+const Token = require('../models/token')
+
+module.exports = async (req, res, next) => {
+        const auth = req.headers?.authorization;
+        const tokenKey = auth ? auth.split(' ') : null
+        if (tokenKey && tokenKey[0] == 'Token') {  //* tokenyapısı >>>> Token ...ashjbdasjhkbdasbdaaln...
+           const tokenData = await Token.findOne({token: tokenKey[1]}).populate('userId')
+           req.user == tokenData ? tokenData.userId : false
+        }
+}
