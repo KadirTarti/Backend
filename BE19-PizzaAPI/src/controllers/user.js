0811f8378ad470ec47d9ec1ce28d4326aa5cb682
tarti <tartilaci@gmail.com>
2024-07-10 22:54:04 +0200

    update usercontroller

--- BE19-PizzaAPI/src/controllers/user.js
+++ BE19-PizzaAPI/src/controllers/user.js
@@ -7,6 +7,19 @@ const User = require("../models/user");
 
 module.exports = {
   list: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can send query with endpoint for filter[], search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
     const data = await res.getModelList(User);
     res.status(200).send({
       error: false,
@@ -16,6 +29,10 @@ module.exports = {
   },
   //! CRUD(Create-Read-Update-Delete)
   create: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+        */
     const data = await User.create(req.body);
     res.status(201).send({
       error: false,
@@ -23,16 +40,59 @@ module.exports = {
     });
   },
   read: async (req, res) => {
-    const data = await User.findOne({ _id: req.params.id });
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+    //* admin değilse her halükarda kullanıcya kendi bilgilerini döndür
+    let customFilter={}
+    if(!req.user.isAdmin){
+      customFilter={_id:req.user._id}
+    }else {
+      customFilter = { _id: req.params.id };
+    }        
+    const data = await User.findOne(customFilter);
+
+    //? admin değilse ve istediği bilgiler kendine ait değilse o zaman kullancıya hata döndür
+    // if(!req.user.isAdmin){
+    //   if(req.params.id != req.user._id){
+    //     throw new CustomError("No permission! you must be admin or own")
+    //   }
+    // }
+
+    // const data = await User.findOne({ _id: req.params.id });
     res.status(200).send({
       error: false,
       data,
     });
   },
   update: async (req, res) => {
-    const data = await User.updateOne({ _id: req.params.id }, req.body, {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+        */
+    let customFilter = {};
+    if (!req.user.isAdmin) {
+      customFilter = { _id: req.user._id }; //* admin değilse değişimi istenen user değil isteği atan userı güncelle
+    } else {
+      customFilter = { _id: req.params.id };
+    }
+    const data = await User.updateOne(customFilter, req.body, {
       runValidators: true,
     });
+
+    //? admin değilse ve istediği bilgiler kendine ait değilse o zaman kullancıya hata döndür
+    // if(!req.user.isAdmin){
+    //   if(req.params.id != req.user._id){
+    //     throw new CustomError("No permission! you must be admin or own")
+    //   }
+    // }
+
+    // const data = await User.updateOne(customFilter, req.body, {
+    //   runValidators: true,
+    // });
+
     res.status(202).send({
       error: false,
       data,
@@ -40,6 +100,10 @@ module.exports = {
     });
   },
   delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
     const data = await User.deleteOne({ _id: req.params.id });
     res.status(data.deletedCount ? 204 : 404).send({
       error: !data.deletedCount,
