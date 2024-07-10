72468c792c3ab7f56242cf447b25322f837cc508
tarti <tartilaci@gmail.com>
2024-07-10 21:22:22 +0200

    "Added Swagger annotations and user-based filtering to Order controller"

--- BE19-PizzaAPI/src/controllers/order.js
+++ BE19-PizzaAPI/src/controllers/order.js
@@ -7,7 +7,27 @@ const Order = require("../models/order");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await res.getModelList(Order);
+      /*
+            #swagger.tags = ["Orders"]
+            #swagger.summary = "List Orders"
+            #swagger.description = `
+                You can send query with endpoint for filter[], search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
+            let customFilter= {}
+
+            if(!req.user.isAdmin){
+              customFilter={userId: req.user._id}
+            }
+            console.log(req.user)
+            const data = await res.getModelList(Order, customFilter);
     res.status(200).send({
       error: false,
       details: await res.getModelListDetails,
@@ -16,7 +36,18 @@ module.exports = {
   },
   //! CRUD(Create-Read-Update-Delete)
   create: async (req, res) => {
+ /*
+            #swagger.tags = ["Orders"]
+            #swagger.summary = "Create Order"
+        */
     // delete req.body.amount - amount alanını db ye eklememek için
+
+    if(!req.user.isAdmin){
+      req.body.userId = req.user._id //* istek atan user 
+    }
+
+    // req.body.userId = req.user._id //* istek atan user 
+    
     const data = await Order.create(req.body);
     res.status(201).send({
       error: false,
@@ -24,6 +55,10 @@ module.exports = {
     });
   },
   read: async (req, res) => {
+       /*
+            #swagger.tags = ["Orders"]
+            #swagger.summary = "Get Single Order"
+        */
     const data = await Order.findOne({ _id: req.params.id });
     res.status(200).send({
       error: false,
@@ -31,6 +66,10 @@ module.exports = {
     });
   },
   update: async (req, res) => {
+    /*
+            #swagger.tags = ["Orders"]
+            #swagger.summary = "Update Order"
+        */
     // delete req.body.amount - amount alanını db ye eklememek için. amount hesaplaması getters ile yapıyoruz
     const data = await Order.updateOne({ _id: req.params.id }, req.body, {
       runValidators: true,
@@ -42,6 +81,10 @@ module.exports = {
     });
   },
   delete: async (req, res) => {
+     /*
+            #swagger.tags = ["Orders"]
+            #swagger.summary = "Delete Order"
+        */
     const data = await Order.deleteOne({ _id: req.params.id });
     res.status(data.deletedCount ? 204 : 404).send({
       error: !data.deletedCount,
