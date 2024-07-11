b9bf52fd5629354366aae17b4ad2ca1ddf186775
tarti <tartilaci@gmail.com>
2024-07-11 14:42:29 +0200

    "Added room controller and middleware to roomRouter.js, and defined routes for CRUD operations on rooms"

--- TEAMWORKS/hotelApi-starter/src/routes/roomRouter.js
+++ TEAMWORKS/hotelApi-starter/src/routes/roomRouter.js
@@ -3,7 +3,20 @@
     EXPRESS - HOTEL API
 ------------------------------------------------------- */
 const router = require("express").Router();
-/* ------------------------------------------------------- */
+
+const room = require('../controllers/roomController')
+const idValidation = require("../middlewares/idValidation");
+
+//^ "/rooms"
+
+router.route("/").get(room.list).post(room.create);
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(room.read)
+  .put(room.update)
+  .patch(room.update)
+  .delete(room.delete);
 
 /* ------------------------------------------------------- */
 module.exports = router;
