a424c13632a6fe91d20596ef4207b956fe241347
tarti <tartilaci@gmail.com>
2024-07-11 09:49:03 +0200

    "Added nodemailer and multer dependencies, and updated index.js to include email functionality using nodemailer"

--- BE19-PizzaAPI/index.js
+++ BE19-PizzaAPI/index.js
@@ -8,6 +8,7 @@
     $ npm init -y
     $ npm i express dotenv mongoose express-async-errors
     $ npm i morgan swagger-autogen swagger-ui-express redoc-express
+    $ npm i nodemailer multer
     $ mkdir logs
     $ nodemon
 */
@@ -54,7 +55,76 @@ app.use(require('./src/middlewares/authentication'))
 //? findSearchSortPage / res.getModelList:
 app.use(require('./src/middlewares/queryHandler'))
 
-/* ------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                    Email                                   */
+/* -------------------------------------------------------------------------- */
+
+//! nodemailer
+// https://nodemailer.com/
+// https://ethereal.email/ (testing email)
+
+const nodemailer = require('nodemailer');
+
+// nodemailer.createTestAccount().then(data=> console.log(data))
+
+/*
+{
+  user: 'uktkuk3eb52y7bxx@ethereal.email',
+  pass: 'mnBTd7xCxsg8b6UnCQ',
+  smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+  imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+  pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
+  web: 'https://ethereal.email',
+  mxEnabled: false
+}
+*/
+
+//* connect nodemailer
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false, // Use `true` for port 465, `false` for all other ports
+//     auth: {
+//       user: "uktkuk3eb52y7bxx@ethereal.email",
+//       pass: "mnBTd7xCxsg8b6UnCQ",
+//     },
+//   });
+
+//   //* send mail
+//  transporter.sendMail({
+//     from: '"Abdulkadir 👻" <uktkuk3eb52y7bxx@ethereal.email>', // sender address
+//     to: "tartilaci@gmail.com", // single user
+//     // to: "bar@example.com, baz@example.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+//   }, (error, success) => {
+//     error ? console.log(error) : console.log(success)
+//   });
+
+
+//* Gmail with nodemailer
+const transporter = nodemailer.createTransport({
+    // host: "smtp.ethereal.email", gmail'de host yerine service kısmı yazılıyor
+    service: "gmail",
+    auth: {
+      user: "tartilaci@gmail.com",
+      pass: "vnui vyvn xpyj thvu",
+    },
+  });
+
+  transporter.sendMail({
+    // from: 'tartilaci@gmail.com',
+    // to: 'tartilaci@gmail.com',
+    text: '',
+    subject: 'Bu bir deneme mesajıdır',
+    text: 'Hello, deneme'
+  }, (error, success) => console.log(success, error))
+
+
+// * ---------------------------------------------------------- * //
+
 // Routes:
 
 // HomePath:
