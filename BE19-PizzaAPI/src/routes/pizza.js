f59ecc27306836ecf945c0aed4798d57e60d09ad
tarti <tartilaci@gmail.com>
2024-07-11 11:01:46 +0200

    "Added multer middleware for file uploads to pizza creation route, updated route definitions and formatting"

--- BE19-PizzaAPI/src/routes/pizza.js
+++ BE19-PizzaAPI/src/routes/pizza.js
@@ -2,15 +2,35 @@
 /* -------------------------------------------------------
     ABDULKADIR TARTILACI - EXPRESS - PIZZA API
 ------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
 
+const router = require("express").Router();
+/* ------------------------------------------------------- */
 const pizza = require("../controllers/pizza");
 const idValidation = require("../middlewares/idValidation");
-const {isAdmin} = require("../middlewares/permissions")
+const { isAdmin } = require("../middlewares/permissions");
+const multer = require("multer");
+
+const upload = multer({
+  // dest: "./uploads",// root
+  storage:multer.diskStorage({
+    destination:"./uploads",
+    filename:function(req,file,returnCallback){
+      // returnCallback(null,"anthony,jpeg")
+      console.log(file)
+      returnCallback(null, Date.now() + "-" + file.originalname)
+    }
+  })
+});
 
 //* /pizzas
-router.route("/").get(pizza.list).post(isAdmin,pizza.create);
+router
+  .route("/")
+  .get(pizza.list)
+  // .post(isAdmin, upload.single("fileInputName"), pizza.create);
+  // .post(isAdmin, upload.single("image"), pizza.create);// single
+  // .post(isAdmin, upload.any(), pizza.create); // not recommended
+  .post(isAdmin, upload.array("images"), pizza.create); // recommended
+  // .post(isAdmin, upload.array("images",10), pizza.create); // recommended
 
 router
   .route("/:id")
@@ -21,4 +41,4 @@ router
   .delete(isAdmin, pizza.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
