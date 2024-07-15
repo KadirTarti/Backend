0c2656cb6689d48159b858105855fa3de24eb13c
tarti <tartilaci@gmail.com>
2024-07-15 11:16:05 +0200

    "Updated passenger controller, model, and route files with new code"

--- BE20-FlightApi/src/routes/passenger.js
+++ BE20-FlightApi/src/routes/passenger.js
@@ -1,4 +1,23 @@
 "use strict"
 /* -------------------------------------------------------
-    NODEJS EXPRESS |  FlightApi | Abdulkadir TARTILACI
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+
+const router = require("express").Router();
+
+//* URL => /passengers
+
+const passenger = require("../controllers/passenger");
+const idValidation = require("../middlewares/idValidation");
+
+router.route("/").get(passenger.list).post(passenger.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(passenger.read)
+  .put(passenger.update)
+  .patch(passenger.update)
+  .delete(passenger.delete);
+
+module.exports = router;
