fcfb9694c7c8536a8121c0ed0e3504ba45637c66
tarti <tartilaci@gmail.com>
2024-07-15 12:09:53 +0200

    "Added permission middleware to flight routes to restrict access based on user role"

--- BE20-FlightApi/src/routes/flight.js
+++ BE20-FlightApi/src/routes/flight.js
@@ -9,15 +9,24 @@ const router = require("express").Router();
 
 const flight = require("../controllers/flight");
 const idValidation = require("../middlewares/idValidation");
+const permission = require("../middlewares/permissions");
 
-router.route("/").get(flight.list).post(flight.create);
+
+//* Login olan herkes uçuşları listeleyebilir.
+//! Staff yada Admin olan post,put,patch işlemlerini yapabilir.
+//? sadece admin delete işlemini yapabilir.
+
+router
+  .route("/")
+  .get(permission.isLogin, flight.list)
+  .post(permission.isStaffOrAdmin, flight.create);
 
 router
   .route("/:id")
   .all(idValidation)
-  .get(flight.read)
-  .put(flight.update)
-  .patch(flight.update)
-  .delete(flight.delete);
+  .get(permission.isLogin, flight.read)
+  .put(permission.isStaffOrAdmin, flight.update)
+  .patch(permission.isStaffOrAdmin, flight.update)
+  .delete(permission.isAdmin, flight.delete);
 
 module.exports = router;
