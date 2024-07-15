01318a6269ee5ebc4b30f64afcd25483c796a2f9
tarti <tartilaci@gmail.com>
2024-07-15 11:48:27 +0200

    "Updated Reservation model schema to allow passengers to be either ObjectId refs or objects with firstName, lastName, and email fields; added required field to createdId"

--- BE20-FlightApi/src/models/reservation.js
+++ BE20-FlightApi/src/models/reservation.js
@@ -1,27 +1,50 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
-    NODEJS EXPRESS |  FlightApi | Abdulkadir TARTILACI
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 
 const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- *
+{
+	"flightId": "652cebb3bae9cde5e8a9753b",
+	"passengers": [
+	  "652cf408b63b905ad13d9a87",
+	  "652cf408b63b905ad13d9a89",
+	  {
+		"firstName": "Test 11",
+		"lastName": "Test 11",
+		"email": "test11@site.com"
+	  },
+	  {
+		"firstName": "Test 12",
+		"lastName": "Test 12",
+		"email": "test12@site.com"
+	  },
+	],
+	"createdId": "652ceaa1bae9cde5e8a97522"
+  }
 /* ------------------------------------------------------- */
 
 const ReservationSchema = new mongoose.Schema(
   {
     flightId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Flight',
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Flight",
+      required: true,
     },
-    passengers: [{
+    // passengers: [],
+    passengers: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Passenger',
+        ref: "Passenger",
         required: true,
-    }],
+      },
+    ],
     createdId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-    }
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     collection: "reservations",
@@ -29,4 +52,4 @@ const ReservationSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("Reservation",ReservationSchema);
+module.exports = mongoose.model("Reservation", ReservationSchema);
