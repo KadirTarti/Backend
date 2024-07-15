a542e95cc9ac3d6a3438f680e803b4b27cac9669
tarti <tartilaci@gmail.com>
2024-07-15 12:14:29 +0200

    "Added permission middleware to passenger routes"

--- BE20-FlightApi/src/routes/passenger.js
+++ BE20-FlightApi/src/routes/passenger.js
@@ -9,15 +9,16 @@ const router = require("express").Router();
 
 const passenger = require("../controllers/passenger");
 const idValidation = require("../middlewares/idValidation");
+const permission = require('../middlewares/permissions')
 
-router.route("/").get(passenger.list).post(passenger.create);
+router.route("/").get(permission.isLogin, passenger.list).post(permission.isLogin, passenger.create);
 
 router
   .route("/:id")
   .all(idValidation)
-  .get(passenger.read)
-  .put(passenger.update)
-  .patch(passenger.update)
-  .delete(passenger.delete);
+  .get(permission.isLogin, passenger.read)
+  .put(permission.isStaffOrAdmin, passenger.update)
+  .patch(permission.isStaffOrAdmin, passenger.update)
+  .delete(permission.isAdmin, passenger.delete);
 
 module.exports = router;
