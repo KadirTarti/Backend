7027545ace07736dc8a4ae11e13fedc4fc4b1a89
tarti <tartilaci@gmail.com>
2024-07-15 11:47:45 +0200

    "Added Reservation model with schema definition to reservation.js file"

--- BE20-FlightApi/src/models/reservation.js
+++ BE20-FlightApi/src/models/reservation.js
@@ -2,3 +2,31 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS |  FlightApi | Abdulkadir TARTILACI
 ------------------------------------------------------- */
+
+const { mongoose } = require("../configs/dbConnection");
+/* ------------------------------------------------------- */
+
+const ReservationSchema = new mongoose.Schema(
+  {
+    flightId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Flight',
+        required: true,
+    },
+    passengers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Passenger',
+        required: true,
+    }],
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    }
+  },
+  {
+    collection: "reservations",
+    timestamps: false,
+  }
+);
+
+module.exports = mongoose.model("Reservation",ReservationSchema);
