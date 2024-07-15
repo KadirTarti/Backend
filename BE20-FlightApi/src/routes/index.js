0d317e1daa2d774f5950fb0b9d9bd08c8adc2940
tarti <tartilaci@gmail.com>
2024-07-15 12:07:49 +0200

    "Added passengers and reservations routes, renamed user route to users, and added semicolon to module.exports"

--- BE20-FlightApi/src/routes/index.js
+++ BE20-FlightApi/src/routes/index.js
@@ -8,18 +8,19 @@ const router = require('express').Router()
 
 // URL: /
 
+
 // auth:
 router.use('/auth', require('./auth'))
-
-// user:
+// users:
 router.use('/users', require('./user'))
-
 // flights:
 router.use('/flights', require('./flight'))
-
-
+// passengers:
+router.use('/passengers', require('./passenger'))
+// reservations:
+router.use('/reservations', require('./reservation'))
 // document:
 router.use('/documents', require('./documents'))
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
