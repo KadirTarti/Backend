c6968f0fafb51944a95c1dec438f38dbbcff1392
tarti <tartilaci@gmail.com>
2024-07-15 10:56:46 +0200

    "Added Express router and routes for flight API, including GET, POST, PUT, PATCH, and DELETE methods, with middleware for ID validation."

--- BE20-FlightApi/src/routes/flight.js
+++ BE20-FlightApi/src/routes/flight.js
@@ -1,4 +1,23 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
-    NODEJS EXPRESS |  FlightApi | Abdulkadir TARTILACI
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+
+const router = require("express").Router();
+
+//* URL => /flights
+
+const flight = require("../controllers/flight");
+const idValidation = require("../middlewares/idValidation");
+
+router.route("/").get(flight.list).post(flight.create);
+
+router
+  .route("/:id")
+  .all(idValidation)
+  .get(flight.read)
+  .put(flight.update)
+  .patch(flight.update)
+  .delete(flight.delete);
+
+module.exports = router;
