0fd16993c8c9eb67ba589ddc3a84bfd98c858000
tarti <tartilaci@gmail.com>
2024-07-17 22:48:56 +0200

    "Updated passenger route with new middleware and permission checks"

--- BE20-FlightApi/src/routes/passenger.js
+++ BE20-FlightApi/src/routes/passenger.js
@@ -6,17 +6,28 @@
 const router = require("express").Router();
 
 //* URL => /passengers
-
+const Passenger = require("../models/passenger")
 const passenger = require("../controllers/passenger");
 const idValidation = require("../middlewares/idValidation");
-const permission = require('../middlewares/permissions')
+const permission = require("../middlewares/permissions")
 
-router.route("/").get(permission.isLogin, passenger.list).post(permission.isLogin, passenger.create);
+//* login olan kullanıcı kendi yolcularını görüntüleyebilir, yolcu oluşturabilir.
+//? Yolcu editleme işlemini staff yada Admin yapabilir.
+//! Yolcu silme işlemini Admin yapabilir.
+const getModel = (req,res,next)=>{
+  req.model = Passenger;
+  next()  
+}
+
+router
+  .route("/")
+  .get(permission.isLogin, passenger.list)
+  .post(permission.isLogin, passenger.create);
 
 router
   .route("/:id")
   .all(idValidation)
-  .get(permission.isLogin, passenger.read)
+  .get(permission.isLogin,getModel, permission.isAdminOrStaffOrOwn, passenger.read)
   .put(permission.isLoginStaffOrAdmin, passenger.update)
   .patch(permission.isLoginStaffOrAdmin, passenger.update)
   .delete(permission.isLoginAdmin, passenger.delete);
