04f9ab29a54d9ef8a0d01586c46ccd85945ca716
tarti <tartilaci@gmail.com>
2024-07-17 22:38:08 +0200

    "Added custom error handling, filtering, and Swagger annotations to passenger controller"

--- BE20-FlightApi/src/controllers/passenger.js
+++ BE20-FlightApi/src/controllers/passenger.js
@@ -1,4 +1,5 @@
 "use strict";
+const { CustomError } = require("../errors/customError");
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
@@ -19,15 +20,29 @@ module.exports = {
                 </ul>
             `
         */
-    const data = await res.getModelList(Passenger);
+    let customFilter = {}
+    if(!req.user.isAdmin && !req.user.isStaff){
+      customFilter = {createdId: req.user._id}
+    }
+    const data = await res.getModelList(Passenger, customFilter);
 
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(Passenger),
+      details: await res.getModelListDetails(Passenger, customFilter),
       data,
     });
   },
   create: async (req, res) => {
+    /*
+            #swagger.tags = ["Passengers"]
+            #swagger.summary = "Create Passenger"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                }
+            }
+        */
     req.body.createdId = req.user._id;
 
     const data = await Passenger.create(req.body);
@@ -38,6 +53,17 @@ module.exports = {
     });
   },
   read: async (req, res) => {
+    /*
+            #swagger.tags = ["Passengers"]
+            #swagger.summary = "Get Single Passenger"
+        */
+    //* isAdminOrStaffOrOwn   
+    // if(!req.user.isAdmin && !req.user.isStaff) {
+    //   const checkData = await Passenger.findOne({_id:req.params.id})
+    //   if(checkData.createdId?.toString() !=  req.user._id.toString()) {
+    //     throw new CustomError("NoPermission!You must be admin or staff or own!")
+    //   }
+    // }
     const data = await Passenger.findOne({ _id: req.params.id }).populate(
       "createdId"
     );
@@ -48,6 +74,16 @@ module.exports = {
     });
   },
   update: async (req, res) => {
+    /*
+            #swagger.tags = ["Passengers"]
+            #swagger.summary = "Update Passenger"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                }
+            }
+        */
     req.body.createdId = req.user._id;
     const data = await Passenger.updateOne({ _id: req.params.id }, req.body, {
       runValidators: true,
@@ -59,6 +95,10 @@ module.exports = {
     });
   },
   delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Passengers"]
+            #swagger.summary = "Delete Passenger"
+        */
     const data = await Passenger.deleteOne({ _id: req.params.id });
 
     res.status(data.deletedCount ? 204 : 404).send({
