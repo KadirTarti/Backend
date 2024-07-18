2da25926c2e86187e1eefac087a9bad5f51fcc3f
tarti <tartilaci@gmail.com>
2024-07-18 11:58:44 +0200

    "Added routes and middleware for reservation API endpoints"

--- BE21-RentACarApi/src/routes/reservation.js
+++ BE21-RentACarApi/src/routes/reservation.js
@@ -5,3 +5,22 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/reservation:
+
+const reservation = require('../controllers/reservation')
+
+const permissions = require('../middlewares/permissions')
+
+// URL: /reservations
+
+router.route('/')
+    .get(permissions.isLogin, reservation.list)
+    .post(permissions.isLogin, reservation.create)
+
+router.route('/:id')
+    .get(permissions.isLogin, reservation.read)
+    .put(permissions.isStaff, reservation.update)
+    .patch(permissions.isStaff, reservation.update)
+    .delete(permissions.isAdmin, reservation.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
