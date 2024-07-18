b80a6e59e1a4ad0c60e99ca9d98a1abd3807b121
tarti <tartilaci@gmail.com>
2024-07-18 13:43:28 +0200

    "Updated env.sample file with default values and modified dbConnection.js to use hardcoded MongoDB connection string"

--- TEAMWORKS/HotelAPI/src/configs/dbConnection.js
+++ TEAMWORKS/HotelAPI/src/configs/dbConnection.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const dbConnection = function () {
   // Connect:
   mongoose
-    .connect(process.env.MONGODB)
+    .connect('mongodb://127.0.0.1:27017/hotelAPI')
     .then(() => console.log("* DB Connected * "))
     .catch((err) => console.log("* DB Not Connected * ", err));
 };
