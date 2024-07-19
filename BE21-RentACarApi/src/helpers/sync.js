ccc0a9797cd08343b4552307f6cd98b07cf80a76
tarti <tartilaci@gmail.com>
2024-07-19 14:18:06 +0200

    "Added code to create admin, staff, and user accounts, as well as a car entry, in sync.js, but it is currently commented out."

--- BE21-RentACarApi/src/helpers/sync.js
+++ BE21-RentACarApi/src/helpers/sync.js
@@ -14,4 +14,52 @@ module.exports = async function() {
     console.log('- Database and all data DELETED!')
     /* REMOVE DATABASE */
 
+    /*
+     await User.create({
+        username: "admin",
+        password: "aA?123456",
+        email: "admin@site.com",
+        firstName: "admin",
+        lastName: "admin",
+        isActive: true,
+        isStaff: true,
+        isAdmin: true
+    })
+    console.log('- Admin user created.')
+
+    await User.create({
+        username: "staff",
+        password: "aA?123456",
+        email: "staff@site.com",
+        firstName: "staff",
+        lastName: "staff",
+        isActive: true,
+        isStaff: true,
+        isAdmin: false
+    })
+    console.log('- Staff user created.')
+
+    await User.create({
+        username: "user",
+        password: "aA?123456",
+        email: "user@site.com",
+        firstName: "user",
+        lastName: "user",
+        isActive: true,
+        isStaff: false,
+        isAdmin: false
+    })
+    console.log('- User created.')
+
+      await Car.create({
+        plateNumber: "34ABC1236",
+        brand: "Opel",
+        model: "Astra",
+        year: 2024,
+        isAutomatic: true,
+        pricePerDay: 249.99
+    })
+    console.log('- Ford Car created.')
+    */
+
 }
