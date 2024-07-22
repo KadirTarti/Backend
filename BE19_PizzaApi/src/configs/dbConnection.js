63654e0d02ceb8986f99c62b273087f8684b0d37
tarti <tartilaci@gmail.com>
2024-07-22 23:50:34 +0200

    "Updated dbConnection.js to use process.env.MONGODBURL instead of hardcoded MongoDB connection string"

--- BE19_PizzaApi/src/configs/dbConnection.js
+++ BE19_PizzaApi/src/configs/dbConnection.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = function() {
     // Connect:
-    mongoose.connect('mongodb://127.0.0.1:27017/pizzaAPI')
+    mongoose.connect(process.env.MONGODBURL)
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
