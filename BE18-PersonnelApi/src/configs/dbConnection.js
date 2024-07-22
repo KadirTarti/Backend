21463c6f5bdd48e94bff0145dd97472e68f6c039
tarti <tartilaci@gmail.com>
2024-07-22 23:49:46 +0200

    "Updated dbConnection.js to use environment variable MONGODBURL instead of hardcoded MongoDB connection strings."

--- BE18-PersonnelApi/src/configs/dbConnection.js
+++ BE18-PersonnelApi/src/configs/dbConnection.js
@@ -9,10 +9,8 @@ const mongoose = require('mongoose')
 
 const dbConnection = function() {
     // Connect:
-    console.log(process.env.MONGODB)
-    mongoose.connect('mongodb://127.0.0.1:27017/personnelAPI')
-    // mongoose.connect('mongodb+srv://abdulkadir:1234@localhost:27017/personelAPI/admin)
-    // mongoose.connect('mongodb+srv://[tarti:1234@]localhost:27017[personnelAPI]')
+    console.log(process.env.MONGODBURL)
+    mongoose.connect(process.env.MONGODBURL)
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
