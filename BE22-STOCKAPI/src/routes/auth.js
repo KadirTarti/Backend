6b07dac57181b8f505556463eabd6c259e57a01b
tarti <tartilaci@gmail.com>
2024-07-22 23:14:30 +0200

    "Deleted auth.js controller file and updated auth.js route file to import from authController"

--- BE22-STOCKAPI/src/routes/auth.js
+++ BE22-STOCKAPI/src/routes/auth.js
@@ -4,7 +4,7 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
-const auth = require("../controllers/auth")
+const auth = require("../controllers/authController")
 
 //* /auth
 
