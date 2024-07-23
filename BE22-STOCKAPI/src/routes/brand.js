e5dd72e66d4d4452c46bb7c00564f788be8f3da7
tarti <tartilaci@gmail.com>
2024-07-23 14:11:22 +0200

    "Removed permission middleware from brand route handlers"

--- BE22-STOCKAPI/src/routes/brand.js
+++ BE22-STOCKAPI/src/routes/brand.js
@@ -24,8 +24,16 @@ router
 router
   .route("/:id")
   .all(idValidation)
-  .get(permission.isLogin, brand.read)
-  .put(permission.isLoginStaffOrAdmin, brand.update)
-  .patch(permission.isLoginStaffOrAdmin, brand.update)
-  .delete(permission.isLoginAdmin, brand.delete);
+  .get(
+    // permission.isLogin, 
+    brand.read)
+  .put(
+    // permission.isLoginStaffOrAdmin, 
+    brand.update)
+  .patch(
+    // permission.isLoginStaffOrAdmin, 
+    brand.update)
+  .delete(
+    // permission.isLoginAdmin, 
+    brand.delete);
 module.exports = router;
