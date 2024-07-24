bf17e84cfce1982264d7c804cf0e68967352f6f1
tarti <tartilaci@gmail.com>
2024-07-24 11:46:23 +0200

    "Added comments to purchase.js controller file"

--- 32-StockApi-2/project/src/controllers/purchase.js
+++ 32-StockApi-2/project/src/controllers/purchase.js
@@ -3,7 +3,14 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Purchase Controller
-
+/* 
+    Stokta 0 ürün var
+    a ürününden 20 adet aldık stokta ürün 20
+    a ürününden 80 adet daha aldık stokta 100
+    a üründünden 10 adet aldık ürün 110
+    10 alınan ürün satın alamaişlemini sildik ürün 100
+    20 adet aldığımız ürün esasen 15 miş düzenleme yaptım ürün 95 
+*/ 
 const Firm = require('../models/purchase')
 
 module.exports = {
