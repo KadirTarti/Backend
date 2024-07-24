aa90023902c231730cf1cb8fcc2cdd737281dd80
tarti <tartilaci@gmail.com>
2024-07-24 11:46:42 +0200

    "Added a comment block to sale.js controller with a scenario description"

--- 32-StockApi-2/project/src/controllers/sale.js
+++ 32-StockApi-2/project/src/controllers/sale.js
@@ -4,6 +4,13 @@
 ------------------------------------------------------- */
 // Sale Controller
 
+/* 
+    Stokta 100 ürün
+    müşteri 10 ader aldı kalan ürün 90
+    10 ürün müşteri 5 ni geri ver iade etti kalan 95
+    diğer müşteri 5 ürün aldı kalan 90
+    bu müşteri yanlışlık olmuş bu satışı sildik kalan 95
+*/ 
 const Firm = require('../models/sale')
 
 module.exports = {
