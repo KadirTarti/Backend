ff3e72a09d9a4b2f3f7d4b1ddf8e4d8ba9c555b9
tarti <tartilaci@gmail.com>
2024-07-24 11:47:15 +0200

    "Updated product creation in sync.js to include price and quantity fields, replacing stock field."

--- 32-StockApi-2/project/src/helpers/sync.js
+++ 32-StockApi-2/project/src/helpers/sync.js
@@ -205,35 +205,40 @@ module.exports = async function () {
         "name": "Tommy",
         "categoryId": "65343222b67e9681f937f203",
         "brandId": "65343222b67e9681f937f107",
-        "stock": 0
+        "price": 99.99,
+        "quantity": 0
     })
     await Product.create({
         "_id": "65343222b67e9681f937f422",
         "name": "Link",
         "categoryId": "65343222b67e9681f937f202",
         "brandId": "65343222b67e9681f937f123",
-        "stock": 910
+        "price": 399.99,
+        "quantity": 910
     })
     await Product.create({
         "_id": "65343222b67e9681f937f423",
         "name": "Cola Turka",
         "categoryId": "65343222b67e9681f937f202",
         "brandId": "65343222b67e9681f937f123",
-        "stock": 750
+        "price": 49.99,
+        "quantity": 750
     })
     await Product.create({
         "_id": "65343222b67e9681f937f426",
         "name": "Rondo",
         "categoryId": "65343222b67e9681f937f201",
         "brandId": "65343222b67e9681f937f123",
-        "stock": 900
+        "price": 299.99,
+        "quantity": 900
     })
     await Product.create({
         "_id": "65343222b67e9681f937f427",
         "name": "Iphone 14 Pro",
         "categoryId": "65343222b67e9681f937f204",
+        "price": 199.99,
         "brandId": "65343222b67e9681f937f131",
-        "stock": 0
+        "quantity": 0
     })
 
     /* Purchase */
