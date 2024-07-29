4bb28e1de11fc29411ce11774ac06b16bfe8bcae
tarti <tartilaci@gmail.com>
2024-07-29 09:42:17 +0200

    "Updated Dockerfile and dbConnection.js: changed base image, added directories, and modified MongoDB connection string"

--- BE23-Docker/docker-2/backend/src/configs/dbConnection.js
+++ BE23-Docker/docker-2/backend/src/configs/dbConnection.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = function () {
     // Connect:
-    mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODBURL, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log('* DB Connected * ')
             require('../helpers/sync')() // !!! It clear database.
