1440699eb56a9cf3a02340c063299d5d5f64aa6e
tarti <tartilaci@gmail.com>
2024-07-30 10:31:21 +0200

    "Updated todoTemplate.js: added comments, changed quote style, and reformatted code"

--- BE24-TodoApp-EJS/src/controllers/todoTemplate.js
+++ BE24-TodoApp-EJS/src/controllers/todoTemplate.js
@@ -1,11 +1,15 @@
-'use strict'
+"use strict";
+/*
+    EXPRESSJS 
+    ! TODO CONTROLLER
+*/
 
-const Todo = require('../models/todoModel')
+const Todo = require("../models/todoModel");
 
 module.exports = {
-    list: async (req, res) => {
+    list:async (req,res) => {
         const data = await Todo.findAndCountAll();
 
-        res.render('todoList', {data})
+        res.render("todoList", {data} )
     }
 }
