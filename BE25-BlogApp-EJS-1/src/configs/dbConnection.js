7074a806f5a2ad59260bff1258837f9b70acc9a7
tarti <tartilaci@gmail.com>
2024-07-31 23:29:20 +0200

    "Updated db connection to use MONGODBURL env variable instead of MONGODB"

--- BE25-BlogApp-EJS-1/src/configs/dbConnection.js
+++ BE25-BlogApp-EJS-1/src/configs/dbConnection.js
@@ -9,6 +9,6 @@ const mongoose = require("mongoose");
 // }
 
 mongoose
-    .connect(process.env.MONGODB)
+    .connect(process.env.MONGODBURL)
     .then(() => console.log("DB Connection"))
     .catch((err) => console.log(err));
