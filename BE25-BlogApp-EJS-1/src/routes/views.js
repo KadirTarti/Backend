f4ae9a835b87133f339e696cf60c811ee4d486c8
tarti <tartilaci@gmail.com>
2024-08-02 11:38:48 +0200

    "Refactored views.js route file: removed unnecessary imports, rearranged route definitions, and reformatted code"

--- BE25-BlogApp-EJS-1/src/routes/views.js
+++ BE25-BlogApp-EJS-1/src/routes/views.js
@@ -2,28 +2,27 @@
 
 const router = require("express").Router();
 
-const { get } = require("mongoose");
-const { BlogPostController: BlogPostControllerView ,BlogCategoryController } = require("../controllers/blogControllerView");
-const { put } = require("./blogRoute");
-// const isAuth = require("../middlewares/isAuth");
+const { BlogPostController:BlogPostControllerView,BlogCategoryController } = require("../controllers/blogControllerView");
+// const isAuth = require("../middlewares/isAuth")
 
-//! base route  =>  /blog
 
-router.all('/', (req, res)=>{
+//? Browserlardan sadce ve sadece GET isteği gelir. HTML Form aracılığıyla sadece GET ve POST isteği gelir. O nedenle route planlaması yaparken diğer istekler için ona uygun bir planlama yapılmalıdır.
+
+//* method olarak get ve post diye de kullanılabilir. Yada all metodu kullanılarak controllerda gelen istek kontrol edilebilir.
+
+
+//! base route => /
+//* / anasayfayı viewe verdiğimiz için projemiz ayaga kalkdığında hatalı sayfa gelecektiir çünkü routeu bulamayacaktır. Çünkü /post şeklinde ayarlama yaptım. Bunun sebebi de ilerleyen süreçlerde yeni routelar eklenebilir diye. Bunun i,çin ana sayfay istek geldiğinde /post a yönlendirme yaptırabilirim:
+router.all('/', (req,res)=>{
   res.redirect('/post')
-});
+})
 
+router.all("/post", BlogPostControllerView.list);
+router.all("/post/create", BlogPostControllerView.create);
+router.all("/post/:postId/update", BlogPostControllerView.update); 
+router.all("/post/:postId", BlogPostControllerView.read); 
+router.all("/post/:postId/delete", BlogPostControllerView.delete); 
 
-router.all('/post', BlogPostControllerView.list);
-router.all('/post/create', BlogPostControllerView.create);
-router.all('/post/:postId/update', BlogPostControllerView.update);
-router.all('/post/:postId', BlogPostControllerView.read);
-router.all('/post/:postId/delete', BlogPostControllerView.delete);
-
-const UserView = require('../controllers/user.controller.view')
-
-router.all('/login', UserView.login);
-router.all('/logout', UserView.logout);
 
 
 module.exports = router;
