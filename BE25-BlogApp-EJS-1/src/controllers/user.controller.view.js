a07f0473bff86e24fea2cbe6c44476c6365d61ac
tarti <tartilaci@gmail.com>
2024-08-02 12:09:46 +0200

    "Updated header.ejs and user.controller.view.js files with minor changes to conditional statements and session handling."

--- BE25-BlogApp-EJS-1/src/controllers/user.controller.view.js
+++ BE25-BlogApp-EJS-1/src/controllers/user.controller.view.js
@@ -4,55 +4,52 @@ const User = require("../models/user.model");
 const passwordEncrpyt = require("../helpers/passwordEncrpyt");
 
 module.exports = {
-
-  
   login: async (req, res) => {
     if(req.method == 'POST'){
-      const {email,password,remindMe} = req.body;
+      const { email, password, remindMe } = req.body;
 
-    if(email && password) {
-      // const user = await User.findOne({email:email})
-      const user = await User.findOne({email})
-      if(user) {
-        // console.log("db: ", user.password)
-        // console.log("user: ",passwordEncrpyt(password));
-        if(user.password == passwordEncrpyt(password)){
+      if (email && password) {
+        // const user = await User.findOne({email:email})
+        const user = await User.findOne({ email });
+        if (user) {
+          // console.log("db: ", user.password)
+          // console.log("user: ",passwordEncrpyt(password));
+          if (user.password == passwordEncrpyt(password)) {
+            /*! Session */
+            //* oturum süresince erişim sağlanır
+            // req.session = {
+            //   email: user.email,
+            //   password:user.password
+            // }
+            req.session.email = user.email;
+            req.session.password = user.password;
+            req.session.id = user._id;
 
-          /*! Session */
-          //* oturum süresince erişim sağlanır
-          // req.session = {
-          //   email: user.email,
-          //   password:user.password
-          // }
-          req.session.email = user.email
-          req.session.password = user.password
-          req.session.id = user._id
-
-          if(remindMe) {
-            req.session.remindMe = remindMe
-            //* sessionu cookieye çeviriyoruz. Verdiğimiz süre kadar erişim sağlanır
-            req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3;
-          }
+            if (remindMe) {
+              req.session.remindMe = remindMe;
+              //* sessionu cookieye çeviriyoruz. Verdiğimiz süre kadar erişim sağlanır
+              req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3;
+            }
             // res.status(200).send({
             //   error: false,
             //   message: "Login Ok!",
             //   user,
             // });
-            req.session.message = null;
-            res.redirect('/');
-        }else {
-          throw new Error("Login parameters not true!");
+            req.session.message = null
+            res.redirect("/");
+          } else {
+            throw new Error("Login parameters not true!");
+          }
+        } else {
+          throw new Error("User not found!");
         }
-      }else {
-        throw new Error("User not found!");
+      } else {
+        throw new Error("Email and password are required!");
       }
     }else {
-      throw new Error("Email and password are required!")
-    }
-    } else {
-      res.render('loginForm', {
+      res.render('loginForm',{
         user:req.session,
-        message: req.session?.message
+        message: req.session?.message,
       })
     }
   },
@@ -62,6 +59,6 @@ module.exports = {
     //   error: false,
     //   message: "Logout Ok!",
     // });
-    res.redirect('/');
+    res.redirect("/");
   },
 };
