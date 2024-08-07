da75ef0c21dbd63198b3839116532e32be692414
tarti <150926891>
2024-08-07 12:14:19 +0200

    "Refactored user controller to add Swagger annotations, modify filtering and authorization logic, and update create, read, update, and delete methods"

--- BE27-BlogAPI/src/controllers/user.controller.js
+++ BE27-BlogAPI/src/controllers/user.controller.js
@@ -9,81 +9,146 @@ const passwordEncrypt = require('../helpers/passwordEncrypt')
 
 module.exports = {
   list: async (req, res) => {
-    const data = await res.getModelList(User,{},"userId");
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+
+            // Sadece kendi kayıtlarını görebilir:
+        // Çalışanlarımız ve Admin tük kullanıcıları görebilir
+        // Şirket politikası na göre isStaff kaldırılabilir
+        const customFilters = (req.user?.isAdmin || req.user?.isStaff) ? {} : { _id: req.user._id }
+        const data = await res.getModelList(User, customFilters)
+
     res.status(200).send({
       error: false,
-      detail: await res.getModelListDetails(User),
+      detail: await res.getModelListDetails(User, customFilters),
       data,
     });
   },
+
   create: async (req, res) => {
-    const isStuff = req.body?.isStuff || false;
-    let message = "Yeni personel eklendi."
-    if (isStuff) {
-      const isUpdated = await User.updateMany(
-        {
-          userId: req.body.userId,
-          isLead: true,
-        },
-        { isLead: false }
-      );
-      console.log(isUpdated)
-      if (isUpdated.modifiedCount) {
-        message = "Önceki Stuff kaldırıldı.Yeni personel eklendi.";
-      }
-    }//* Her takımın tek bir lideri olmak zorunda
+     /*
+          #swagger.tags = ["Users"]
+          #swagger.summary = "Create User"
+          #swagger.parameters['body'] = {
+              in: 'body',
+              required: true,
+              schema: {
+                  "username": "test",
+                  "password": "1234",
+                  "email": "test@site.com",
+                  "firstName": "test",
+                  "lastName": "test",
+              }
+          }
+      */
+       // Yeni kayıtlarda admin/staff = false
+       req.body.isStaff = false
+       req.body.isAdmin = false
 
-    const data = await User.create(req.body);
+       console.log('create before');
+       const data = await User.create(req.body)
+       console.log('create after');
+ 
+          /* AUTO LOGIN *
+        const tokenData = await Token.create({
+            userId: data._id,
+            token: passwordEncrypt(data._id + Date.now())
+        })
+        /* AUTO LOGIN */
 
+    
     res.status(201).send({
       error: false,
-      data,
-      message
+      token: tokenData.token,
+      data
     });
   },
-  read: async (req, res) => {
-    const data = await User.findOne({ _id: req.params.id });
-    res.status(200).send({
-      error: false,
-      data,
-    });
-  },
-  update: async (req, res) => {
-    if(!req.user.isAdmin) {
-      req.body.isAdmin = false;
-      delete req.body.isLead
-      delete req.body.salary
-      delete req.body.title
-      delete req.body.startedAt
-      delete req.body.isActive
-    }
-    const isLead = req.body?.isLead || false;
-    if (isLead) {
-      const { departmentId } = await User.findOne({ _id: req.params.id });
-      console.log(departmentId);
-      await User.updateMany(
-        {
-          departmentId,
-          isLead: true,
-        },
-        { : false }
-      );
-    }
-    const data = await User.updateOne({ _id: req.params.id }, req.body, {
-      runValidators: true, //* modelde var olan validate fonksiyonlarının(built-in ve custom) update işlemi sırasında çalışmasını sağlayan özellik ***
-    });
-    res.status(202).send({
-      error: false,
-      data,
-      newData: await User.findOne({ _id: req.params.id }),
-    });
-  },
-  delete: async (req, res) => {
-    const data = await User.deleteOne({ _id: req.params.id });
 
-    res.status(data.deletedCount > 0 ? 204 : 404).send({
-      error: !data.deletedCount,
-      data,
-    });
+  read: async (req, res) => {
+      /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+    // Sadece kendi kayıtını görebilir:
+        // Çalışanlarımız ve Admin tük kullanıcıları görebilir
+        const customFilters = (req.user?.isAdmin || req.user?.isStaff) ? {} : { _id: req.user._id }
+
+        const data = await User.findOne(customFilters)
+
+        res.status(200).send({
+            error: false,
+            data
+        })
   },
-};
+
+  update: async (req, res) => {
+     /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+   
+             // Sadece kendi kaydını güncelleyebilir:
+        //const customFilters = req.user?.isAdmin ? { _id: req.params.id } : { _id: req.user._id }
+        const customFilters = { _id: req.params.id }
+
+
+    // Yeni kayıtlarda admin/staff durumunu değiştiremez:
+        if (!req.user?.isAdmin) {
+            delete req.body.isStaff
+            delete req.body.isAdmin
+        }
+
+ 
+        const data = await User.updateOne(customFilters, req.body, { runValidators: true })
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await User.findOne(customFilters),
+        })
+    },
+
+  delete: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
+            if (req.params.id !== req.user._id) {
+                const data = await User.deleteOne({ _id: req.params.id })
+                const count = data?.deletedCount ?? 0
+    
+                console.log('delete >> ', count);
+                res.status(count > 0 ? 204 : 404).send({
+                    error: count !== 0,
+                    data
+                })
+            } else {
+                // Admin kendini silemez.
+                res.errorStatusCode = 403
+                throw new Error('You can not remove your account.')
+            }
+        }
+    }
