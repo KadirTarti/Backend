5b7c5fe1a21f2f7529a5c4620e62bfe891b32e36
tarti <150926891>
2024-08-07 10:21:25 +0200

    "Added DB connection logging and exported mongoose and dbConnection"

--- BE27-BlogAPI/src/configs/dbConnection.js
+++ BE27-BlogAPI/src/configs/dbConnection.js
@@ -1,4 +1,7 @@
 
 const dbConnection = function () {
     console.log(process.env.MONGODBURL)
+    .then(() => console.log('* DB Connected *')).catch((err)=> console.log(' * DB Not Connected *', err))
 }
+
+module.exports = {mongoose, dbConnection}
