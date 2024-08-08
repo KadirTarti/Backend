497d8bbf6110972dd77ef597f584d64dc61a8606
tarti <150926891>
2024-08-08 14:00:12 +0200

    "added blog.route.js and routing for CRUD"

---
+++ BE27-BlogAPI/src/routes/blog.route.js
@@ -0,0 +1,27 @@
+"use strict"
+/* -------------------------------------------------------
+    AKT - BlogApi
+------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/blog:
+
+const permissions = require('../middlewares/permissions')
+const blog = require('../controllers/blog.controller')
+
+// URL: /categories
+
+router.route('/')
+    // .get(permissions.isAdmin, blog.list)
+    //? blog.list içinde isAdmin kontrolü yaptık:
+    .get(permissions.isLogin, blog.list)
+    .post(permissions.isLogin, blog.create)
+
+router.route('/:id')
+    .get(permissions.isLogin, blog.read)
+    .put(permissions.isLogin, blog.update)
+    .patch(permissions.isLogin, blog.update)
+    .delete(permissions.isAdmin, blog.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
