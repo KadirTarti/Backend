9b946f9fbac4e66e47b4a5774ff3daee6f137711
tarti <150926891>
2024-08-09 09:02:16 +0200

    "Refactored comment controller: removed unnecessary comments and whitespace, reformatted code, and simplified logic in create, read, update, and delete methods."

--- BE27-BlogAPI/src/controllers/comment.controller.js
+++ BE27-BlogAPI/src/controllers/comment.controller.js
@@ -21,11 +21,7 @@ module.exports = {
             `
         */
 
-            // Sadece kendi kayıtlarını görebilir:
-        // Çalışanlarımız ve Admin tük kullanıcıları görebilir
-        // Şirket politikası na göre isStaff kaldırılabilir
-
-        const data = await res.getModelList(Comment)
+    const data = await res.getModelList(Comment);
 
     res.status(200).send({
       error: false,
@@ -35,54 +31,48 @@ module.exports = {
   },
 
   create: async (req, res) => {
-     /*
+    /*
           #swagger.tags = ["comments"]
           #swagger.summary = "Create comment"
           #swagger.parameters['body'] = {
               in: 'body',
               required: true,
               schema: {
-                  "commentname": "test",
-                  "password": "1234",
-                  "email": "test@site.com",
-                  "firstName": "test",
-                  "lastName": "test",
-              }
+  "blogId": "65343222b67e9681f937f201",
+  "comment": "Comment 1"              }
           }
       */
-           
-       if (user.isActive) {
-           const data = await Comment.create(req.body)
-           res.status(201).send({
-             error: false,
-             data
-           });
-       } else {
-        res.errorStatusCode = 403
-        throw new Error('No permission. You must login!')
-       }
- 
-    
-      },
+
+    if (user.isActive) {
+      const data = await Comment.create(req.body);
+      res.status(201).send({
+        error: false,
+        data,
+      });
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("No permission. You must login!");
+    }
+  },
 
   read: async (req, res) => {
-      /*
+    /*
             #swagger.tags = ["comments"]
             #swagger.summary = "Get Single comment"
         */
 
-        const customFilters = (req.user?.isActive) ? {} : { id: req.user.id }
+    const customFilters = req.user?.isActive ? {} : { id: req.user.id };
 
-        const data = await Comment.findOne(customFilters)
+    const data = await Comment.findOne(customFilters);
 
-        res.status(200).send({
-            error: false,
-            data
-        })
+    res.status(200).send({
+      error: false,
+      data,
+    });
   },
 
   update: async (req, res) => {
-     /*
+    /*
             #swagger.tags = ["comments"]
             #swagger.summary = "Update comment"
             #swagger.parameters['body'] = {
@@ -97,38 +87,40 @@ module.exports = {
                 }
             }
         */
-   
-             // Sadece kendi kaydını güncelleyebilir:
-        //const customFilters = req.comment?.isAdmin ? { _id: req.params.id } : { _id: req.comment._id }
-        const customFilters = { _id: req.params.id }
 
-        const data = await Comment.updateOne(customFilters, req.body, { runValidators: true })
+    // Sadece kendi kaydını güncelleyebilir:
+    //const customFilters = req.comment?.isAdmin ? { _id: req.params.id } : { _id: req.comment._id }
+    const customFilters = { _id: req.params.id };
 
-        res.status(202).send({
-            error: false,
-            data,
-            new: await Comment.findOne(customFilters),
-        })
-    },
+    const data = await Comment.updateOne(customFilters, req.body, {
+      runValidators: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Comment.findOne(customFilters),
+    });
+  },
 
   delete: async (req, res) => {
-        /*
+    /*
             #swagger.tags = ["comments"]
             #swagger.summary = "Delete comment"
         */
-            if (req.params.id !== req.comment._id) {
-                const data = await Comment.deleteOne({ _id: req.params.id })
-                const count = data?.deletedCount ?? 0
-    
-                console.log('delete >> ', count);
-                res.status(count > 0 ? 204 : 404).send({
-                    error: count !== 0,
-                    data
-                })
-            } else {
-                // Admin kendini silemez.
-                res.errorStatusCode = 403
-                throw new Error('You can not remove your account.')
-            }
-        }
+    if (req.params.id !== req.comment._id) {
+      const data = await Comment.deleteOne({ _id: req.params.id });
+      const count = data?.deletedCount ?? 0;
+
+      console.log("delete >> ", count);
+      res.status(count > 0 ? 204 : 404).send({
+        error: count !== 0,
+        data,
+      });
+    } else {
+      // Admin kendini silemez.
+      res.errorStatusCode = 403;
+      throw new Error("You can not remove your account.");
     }
+  },
+};
