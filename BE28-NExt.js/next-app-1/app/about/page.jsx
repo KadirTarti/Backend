9e06d07465a4a8cde084822fd26e99ea298ce9b0
tarti <tartilaci@gmail.com>
2024-08-12 10:43:55 +0200

    "added page jsx and home.jsx to learn route system in nextjs"

---
+++ BE28-NExt.js/next-app-1/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center text-3xl mt-10'>
+    <h1>About Page</h1>
+    </div>
+  )
+}
+  
+
+
+export default page;
+
--- BE28-NExt.js/next-app-1/app/page.js
+++ BE28-NExt.js/next-app-1/app/page.js
@@ -1,3 +1,5 @@
+//^app folder içindeki page.js bizim home (anma) route'umuz..!
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
