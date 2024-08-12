b7df0423ef16605012d830621b326430b18c568d
tarti <tartilaci@gmail.com>
2024-08-12 11:15:22 +0200

    "added ul in navbar with classes"

--- BE28-NExt.js/next-app-1/components/Navbar.jsx
+++ BE28-NExt.js/next-app-1/components/Navbar.jsx
@@ -17,9 +17,18 @@ const navigation = [
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="bg-navbarColor text-sm px-4 flex justify-between">
+    <div className="flex items-center">
       <div>
-        <img src="/clarusway-logo.png" alt="" />
+        <img src="/clarusway-logo.png" width='150px' alt="" />
+      </div>
+      <ul ckassName='flex'>
+        {navigation.map((item)=>(
+            <li key={item.title} className={`font-medium hover:bg-gray-300 rounded-full py-2 px-4 hover:text-white inline-block text-lg`}>
+                <a href={item.path}> {item.title}</a>
+            </li>
+        ))}
+      </ul>
       </div>
     </nav>
   );
