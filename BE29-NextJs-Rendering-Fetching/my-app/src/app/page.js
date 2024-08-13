e5329326416be139fe31501ee0285795c792a8ab
tarti <tartilaci@gmail.com>
2024-08-13 11:12:52 +0200

    "added button css in global-css with tailwind"

--- BE29-NextJs-Rendering-Fetching/my-app/src/app/page.js
+++ BE29-NextJs-Rendering-Fetching/my-app/src/app/page.js
@@ -1,9 +1,11 @@
+import ServerComponent from "@/components/ServerComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>Section 02</h1>
+    <h1>Hi! Here is Dashboard Page</h1>
+    <ServerComponent/>
     </main>
   );
 }
---
+++ BE29-NextJs-Rendering-Fetching/my-app/src/components/ServerComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ServerComponent = () => {
+    console.log("server comp runs on server side")
+  return (
+    <div>ServerComponent</div>
+  )
+}
+
+export default ServerComponent
