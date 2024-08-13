eb60950babe47e538ee1b146236fd97fc4cbda33
tarti <tartilaci@gmail.com>
2024-08-13 11:13:43 +0200

    "Added ClientComponent import and usage, uncommented ServerComponent"

--- BE29-NextJs-Rendering-Fetching/my-app/src/app/page.js
+++ BE29-NextJs-Rendering-Fetching/my-app/src/app/page.js
@@ -1,11 +1,17 @@
 import ServerComponent from "@/components/ServerComponent";
+import ClientComponent from "@/components/ClientComponent";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h1>Hi! Here is Dashboard Page</h1>
+    {/* <ServerComponent /> */}
     <ServerComponent/>
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
     </main>
   );
 }
+
