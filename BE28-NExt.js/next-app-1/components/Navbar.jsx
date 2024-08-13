66a34c6b94d020b9e1151f09aabd67383ed26d50
tarti <tartilaci@gmail.com>
2024-08-13 07:55:23 +0200

    "Updated Navbar component to use next/link for navigation and wrapped logo with Link component"

--- BE28-NExt.js/next-app-1/components/Navbar.jsx
+++ BE28-NExt.js/next-app-1/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const navigation = [
@@ -18,17 +19,23 @@ const navigation = [
 const Navbar = () => {
   return (
     <nav className="bg-navbarColor text-sm px-4 flex justify-between">
-    <div className="flex items-center">
-      <div>
-        <img src="/clarusway-logo.png" width='150px' alt="" />
-      </div>
-      <ul className='flex'>
-        {navigation.map((item)=>(
-            <li key={item.title} className={`font-medium hover:bg-gray-300 rounded-full py-2 px-4 hover:text-white inline-block text-lg`}>
-                <a href={item.path}> {item.title}</a>
+      <div className="flex items-center">
+        <div className="flex items-center py-5 mr-4 ">
+          <Link href="/" className="flex items-center">
+            <img src="/clarusway-logo.png" width="150px" alt="" />
+          </Link>
+        </div>
+        <ul className="flex">
+          {navigation.map((item) => (
+            <li
+              key={item.title}
+              className={`font-medium hover:bg-gray-300 rounded-full py-2 px-4 hover:text-white inline-block text-lg`}
+            >
+              {/* //? next/link arka planda sayfayı önceden fetch edilir. Bu, client tarafı gezintilerin performansını iyileştirmek için kullanışlıdır. Görünüm alanındaki herhangi bir <Link />  önceden yüklenecektir. */}
+              <Link href={item.path}> {item.title} </Link>
             </li>
-        ))}
-      </ul>
+          ))}
+        </ul>
       </div>
     </nav>
   );
