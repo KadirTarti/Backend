cbd907618e30a56b02538358e52436a7673dc097
tarti <tartilaci@gmail.com>
2024-08-13 10:48:18 +0200

    "navbar moved in layout js"

--- BE29-NextJs-Rendering-Fetching/my-app/src/app/layout.js
+++ BE29-NextJs-Rendering-Fetching/my-app/src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      {children}
-      <Navbar/>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
