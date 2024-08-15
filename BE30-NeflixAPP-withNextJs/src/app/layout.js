4a529abe8468eeb1e74f603d67771a225f240656
tarti <tartilaci@gmail.com>
2024-08-15 12:17:35 +0200

    "Updated layout.js: fixed Navbar import typo, added ToastContainer, and reformatted code"

--- BE30-NeflixAPP-withNextJs/src/app/layout.js
+++ BE30-NeflixAPP-withNextJs/src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbvar";
-import 'react-toastify/dist/ReactToastify.css'
+import Navbar from "@/components/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navbar/>
-      {children}</body>
+        <Navbar />
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
