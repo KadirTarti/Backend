d0e4324ddb314a0506bb1db3e9d1bc172c4fae4b
tarti <tartilaci@gmail.com>
2024-08-15 12:18:33 +0200

    "Updated Register page with form handling and authentication calls"

--- BE30-NeflixAPP-withNextJs/src/app/(auth)/register/page.jsx
+++ BE30-NeflixAPP-withNextJs/src/app/(auth)/register/page.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import GoogleIcon from "@/../public/icons/GoogleIcon";
+"use client";
+import React, { useState } from "react";
+import GoogleIcon from "/public/icons/GoogleIcon";
+import useAuthCalls from "@/hooks/useAuthCalls";
 
 const Register = () => {
+  const [info, setInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const { createUser } = useAuthCalls();
+
+  const { email, password, firstName, lastName } = info;
+
+  const handleChange = (e) =>
+    setInfo({ ...info, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const displayName = `${firstName} ${lastName}`;
+    createUser(email, password, displayName);
+  };
+
   return (
-    <main className="h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black w-full h-full bg-opacity-50">
-        <div className="bg-black bg-opacity-70 rounded-md p-16 relative top-28 lg:w-2/5 lg:max-w-md w-full mx-auto">
-          <form>
-            <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
+    <main className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <div className="bg-black bg-opacity-70 p-16 self-center relative top-28 lg:w-2/5 lg:max-w-md rounded-md w-full mx-auto">
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3 ">
               Sign Up
             </h2>
             <div className="relative z-0 w-full mb-6 group">
@@ -15,47 +36,53 @@ const Register = () => {
                 type="text"
                 className="peer"
                 required
-                placeholder=""
+                placeholder=" "
                 name="firstName"
+                onChange={handleChange}
               />
-              <label htmlFor="firstName"> First Name</label>
+              <label htmlFor="firstName">First Name</label>
             </div>
-
             <div className="relative z-0 w-full mb-6 group">
               <input
                 type="text"
                 className="peer"
                 required
-                placeholder=""
+                placeholder=" "
                 name="lastName"
+                onChange={handleChange}
               />
-              <label htmlFor="lastName"> Last Name</label>
+              <label htmlFor="lastName">Last Name</label>
             </div>
-
-
             <div className="relative z-0 w-full mb-6 group">
               <input
                 type="email"
                 className="peer"
                 required
-                placeholder=""
+                placeholder=" "
                 name="email"
+                onChange={handleChange}
               />
-              <label htmlFor="email"> Email</label>
+              <label htmlFor="email">Email</label>
             </div>
-
             <div className="relative z-0 w-full mb-6 group">
               <input
                 type="password"
                 className="peer"
                 required
-                placeholder=""
+                placeholder=" "
                 name="password"
+                onChange={handleChange}
               />
-              <label htmlFor="password"> Password</label>
+              <label htmlFor="password">Password</label>
             </div>
-            <button className="btn-danger bg-opacity-70 ">Register</button>
-            <button className="btn-danger flex justtify-center gap-3 text-center bg-opacity-70"><GoogleIcon/> Continue with Google </button>
+            <button className="btn-danger">Register</button>
+            <button
+              type="button"
+              className="btn-danger flex justify-between text-center "
+            >
+              Continue with Google
+              <GoogleIcon />
+            </button>
           </form>
         </div>
       </div>
--- BE30-NeflixAPP-withNextJs/src/hooks/useAuthCalls.jsx
+++ BE30-NeflixAPP-withNextJs/src/hooks/useAuthCalls.jsx
@@ -1,11 +1,23 @@
-import auth from "@/auth/firebase";
+import { auth } from "@/auth/firebase";
+import { toastErrorNotify, toastSuccessNotify } from "@/helpers/ToastNotify";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const useAuthCalls = () => {
-  const createUser = async (email, password) => {
+  const router = useRouter();
+  const createUser = async (email, password, displayName) => {
     try {
-      createUserWithEmailAndPassword(auth, email, password);
-    } catch (error) {}
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      await createUserWithEmailAndPassword(auth, email, password);
+      //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+      await updateProfile(auth.currentUser, {
+        displayName,
+      });
+      toastSuccessNotify("Registered successfully!");
+      router.push("/login");
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
   };
   return { createUser };
 };
