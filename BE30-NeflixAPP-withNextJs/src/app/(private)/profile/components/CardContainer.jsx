e6f6a03da3a470461401b80bac8a7663f0d36a0c
tarti <tartilaci@gmail.com>
2024-08-16 10:03:47 +0200

    "Formatted code in CardContainer and ProfileCard components, added quotes around import statements and property values, and removed trailing whitespace."

--- BE30-NeflixAPP-withNextJs/src/app/(private)/profile/components/CardContainer.jsx
+++ BE30-NeflixAPP-withNextJs/src/app/(private)/profile/components/CardContainer.jsx
@@ -1,25 +1,32 @@
-"use client"
-import React from 'react'
-import ProfileCard from './ProfileCard'
-import { useSelector } from 'react-redux';
+"use client";
+import React from "react";
+import ProfileCard from "./ProfileCard";
+import { useSelector } from "react-redux";
 
 const profileImages = [
-    "/images/default-blue.png",
-    "/images/default-red.png",
-    "/images/default-slate.png",
-    "/images/default-green.png",
-  ];
+  "/images/default-blue.png",
+  "/images/default-red.png",
+  "/images/default-slate.png",
+  "/images/default-green.png",
+];
 
 const CardContainer = () => {
-    const {currentUser}= useSelector(state => state.auth)
+  const { currentUser } = useSelector((state) => state.auth);
   return (
-    <div className='flex flex-col md:flex-row'>
-    {profileImages.map((image, index) => {
-        
-    <ProfileCard key={index} image={image} name={index == 0 && currentUser ? currentUser?.display : `Guest-${index+1}`}/>
-    })}
+    <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-10">
+      {profileImages.map((image, index) => (
+        <ProfileCard
+          key={index}
+          image={image}
+          name={
+            index == 0 && currentUser
+              ? currentUser?.displayName
+              : `Guest-${index + 1}`
+          }
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CardContainer
+export default CardContainer;
--- BE30-NeflixAPP-withNextJs/src/app/(private)/profile/components/ProfileCard.jsx
+++ BE30-NeflixAPP-withNextJs/src/app/(private)/profile/components/ProfileCard.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const ProfileCard = ({name, image}) => {
+const ProfileCard = ({ name, image }) => {
   return (
-    <div className='w-44 mx-auto cursor-pointer group'>
-        <div className='w-44 h-44'>
-          <img src={image} alt="profile" />
-        </div>
-        <div>
-            {name}
-        </div>
+    <div className="w-44 mx-auto cursor-pointer group">
+      <div className="w-44 h-44 ">
+        <img src={image} alt="profile" />
+      </div>
+      <div>{name}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
