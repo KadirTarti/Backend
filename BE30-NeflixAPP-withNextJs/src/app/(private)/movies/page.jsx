6bfee24db103868c51d44b1dbdaf1709243ae45e
tarti <tartilaci@gmail.com>
2024-08-16 12:19:49 +0200

    "Updated Movies component to import getMovies function, added HeroSection component, and modified JSX structure"

--- BE30-NeflixAPP-withNextJs/src/app/(private)/movies/page.jsx
+++ BE30-NeflixAPP-withNextJs/src/app/(private)/movies/page.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import HeroSection from "./components/HeroSection";
 
-const Movies = () => {
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  //   console.log(movies);
   return (
     <div>
-    
+      <HeroSection
+        title={movies[0].title}
+        overview={movies[0].overview}
+        id={movies[0].id}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
