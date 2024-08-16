4b6fb46d72f6b63fd051029847cd642a27bc58a8
tarti <tartilaci@gmail.com>
2024-08-16 12:20:32 +0200

    Remove `color: white` from `body` in `globals.css`.

---
+++ BE30-NeflixAPP-withNextJs/src/helpers/MovieFunctions.js
@@ -0,0 +1,28 @@
+const API_KEY = process.env.TMDB_KEY;
+
+export const getMovies = async (type) => {
+  const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const { results } = await res.json();
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getVideoKey = async (movieId) => {
+  const videoUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`;
+  try {
+    const res = await fetch(videoUrl);
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const data = await res.json();
+    return data?.results[0]?.key;
+  } catch (error) {
+    console.log(error);
+  }
+};
