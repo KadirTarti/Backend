a56cf254a0783a76b73566706291978fce0ab493
tarti <tartilaci@gmail.com>
2024-08-16 12:23:26 +0200

    "Added MovieSection component and imported it; added four instances of MovieSection with different titles and types"

--- BE30-NeflixAPP-withNextJs/src/app/(private)/movies/page.jsx
+++ BE30-NeflixAPP-withNextJs/src/app/(private)/movies/page.jsx
@@ -1,6 +1,7 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
@@ -12,6 +13,12 @@ const Movies = async () => {
         overview={movies[0].overview}
         id={movies[0].id}
       />
+      <div>
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMING" type="upcoming" />
+      </div>
     </div>
   );
 };
