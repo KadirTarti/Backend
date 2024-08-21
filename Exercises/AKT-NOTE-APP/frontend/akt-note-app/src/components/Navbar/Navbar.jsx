c26fc0347e4942f40a006df37393492a0cf7a1c0
tarti <tartilaci@gmail.com>
2024-08-21 11:08:50 +0200

    "Updated Navbar component and added login form to Login page"

--- Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/components/Navbar/Navbar.jsx
+++ Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
+    <h2 className='text-xl font-medium text-black py-2'></h2>
+    </div>
   )
 }
 
--- Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/pages/login/Login.jsx
+++ Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/pages/login/Login.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom"
 import Navbar from "../../components/Navbar/Navbar"
 
 const Login = () => {
   return (
     <>
       <Navbar/>
+
+      <div>
+        <div>
+          <form onSubmit={()=> {}} action="">
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input type="text" placeholder="Email" className="input-box" />
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet?{''}
+              <Link to='register' className=''>
+                Create a new Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
     )
 }
