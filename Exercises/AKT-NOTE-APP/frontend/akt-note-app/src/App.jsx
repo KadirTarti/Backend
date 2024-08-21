0c4463a498970f154f0547ea084daaedca7b6798
tarti <tartilaci@gmail.com>
2024-08-21 10:54:47 +0200

    "Added react-router-dom dependency and implemented routing in App component"

--- Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/App.jsx
+++ Exercises/AKT-NOTE-APP/frontend/akt-note-app/src/App.jsx
@@ -1,10 +1,23 @@
 
-import Home from './pages/home/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/Register/Register';
+
+const AppRoutes = () => {
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </Router>
+}
 
 const App = () => {
   return (
     <div>
-    <Home/>
+    <AppRoutes/>
     </div>
   )
 }
